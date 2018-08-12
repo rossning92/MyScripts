431acfb71e3705eb131b2b6c5b346655368127a3
Ross Ning <rossning92@gmail.com>
2018-08-12 09:40:58 +0800

    message

--- scripts/Ross/ConvertFlagsToMetaFile.py
+++ scripts/Ross/ConvertFlagsToMetaFile.py
@@ -7,11 +7,10 @@ os.chdir('..')
 patt = r'\[([a-zA-Z_][a-zA-Z_0-9]*)\]'
 
 for f in glob.glob('**/*.*'):
-    print(f)
-
     file_name = os.path.basename(f)
 
     flags = set(re.findall(patt, f))
 
-    file_name = re.sub(patt, '', file_name)
-    print(file_name)
+    if len(flags) > 0:
+        file_name = re.sub(patt, '', file_name)
+        print(file_name)
