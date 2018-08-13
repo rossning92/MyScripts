21707d1bd7fe7a1d943cb92fd5b082dfa3c2f1e8
Ross Ning <rossning92@gmail.com>
2018-08-12 23:55:18 -0700

    message

---
+++ scripts/Ross/Dev/Miniconda.cmd
@@ -0,0 +1,4 @@
+call C:\tools\miniconda3\Scripts\activate.bat
+
+cd /d "{{STARTUP_DIR}}"
+start
--- scripts/Ross/Dev/Setup_DevEnv.cmd
+++ scripts/Ross/Dev/Setup_DevEnv.cmd
@@ -1,3 +1,8 @@
 choco install notepadplusplus -y
 choco install miniconda3 -y
+
 choco install git -y
+git config --global user.email "rossning92@gmail.com"
+git config --global user.name "Ross Ning"
+
+choco install googlechrome -y
