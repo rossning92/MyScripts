54cd53f11b041f1c16976e1404b75e8539705f29
Ross Ning <rossning92@gmail.com>
2018-08-17 10:55:19 +0800

    message

--- scripts/common/OpenScriptFolder.py
+++ scripts/common/OpenScriptFolder.py
@@ -4,6 +4,10 @@ import os
 
 os.chdir('../..')
 
+script_path = os.environ['ROSS_SELECTED_SCRIPT_PATH']
+script_dir = os.path.dirname(script_path)
+os.chdir(script_dir)
+
 if sys.platform == 'darwin':
     subprocess.call('open .', shell=True)
 elif sys.platform == 'win32':
