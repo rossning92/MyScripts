621d2e1ebf15301426ad18f6c0d4a4204c9ae146
Ross Ning <rossning92@gmail.com>
2018-08-28 10:43:50 -0700

    message

--- run.cmd
+++ run.cmd
@@ -6,7 +6,10 @@ set QT_QPA_PLATFORM_PLUGIN_PATH=
 set QML_IMPORT_PATH=
 set QML2_IMPORT_PATH=
 
-set PATH=C:\Python36;C:\Python36\Scripts;%LOCALAPPDATA%\Programs\Python\Python36;%LOCALAPPDATA%\Programs\Python\Python36\Scripts;%PATH%
+
+set CONDA_PATH=%LOCALAPPDATA%\Continuum\anaconda3
+
+set PATH=%CONDA_PATH%;%CONDA_PATH%\Scrtips;C:\Python36;C:\Python36\Scripts;%LOCALAPPDATA%\Programs\Python\Python36;%LOCALAPPDATA%\Programs\Python\Python36\Scripts;%PATH%
 
 where python
 if not %errorlevel%==0 (
