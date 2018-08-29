5c1ba4f2484f3e73c12f317b620aeb6fc0eb798a
Ross Ning <rossning92@gmail.com>
2018-08-29 10:16:41 +0800

    message

--- scripts/ext/open_script_folder.py
+++ scripts/ext/open_script_folder.py
@@ -5,9 +5,9 @@ import os
 script_path = os.environ['ROSS_SELECTED_SCRIPT_PATH']
 script_dir = os.path.dirname(script_path)
 os.chdir('../../' + script_dir)
-print(os.getcwd())
+print('Open Folder: ' + os.getcwd())
 
 if sys.platform == 'darwin':
     subprocess.call('open .', shell=True)
 elif sys.platform == 'win32':
-    subprocess.call('start .', shell=True)
+    subprocess.call('cmd /c start ' + os.getcwd())
