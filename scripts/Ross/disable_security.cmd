cf89680059f5c4ce514752efc610b22eac516403
Ross Ning <rossning92@gmail.com>
2018-08-30 13:49:22 +0800

    message

---
+++ scripts/Ross/disable_security.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+:: Disable Defender
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+
+:: Disable UAC
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+
+:: Disable Firewall
+NetSh Advfirewall set allprofiles state off
+
+pause
