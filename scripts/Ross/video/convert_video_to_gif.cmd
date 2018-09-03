2d6e41af163969b5fa3eb71f979212da50cd2968
Ross Ning <rossning92@gmail.com>
2018-09-03 14:15:21 +0800

    message

--- scripts/Ross/video/convert_video_to_gif.cmd
+++ scripts/Ross/video/convert_video_to_gif.cmd
@@ -1,2 +1,12 @@
-ffmpeg -i output2.mp4 -filter_complex "[0:v] fps=12,scale=w=480:h=-1,split [a][b];[a] pa
-lettegen=stats_mode=single [p];[b][p] paletteuse=new=1" StickAroundPerFrame.gif
+cd /d "%USERPROFILE%\Desktop"
+
+set INPUT=output.mp4
+
+:: set PATH=C:\ProgramData\chocolatey\bin;%PATH%
+
+:: Convert video to gif
+ffmpeg -i %INPUT% -filter_complex "[0:v] fps=25,scale=w=480:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" output.gif
+
+
+:: Optimize gif
+magick output.gif -coalesce -fuzz 4% +dither -layers Optimize output_optimized.gif
