b94d77dae00cf86183d73144ce1fa32cd4bea672
Ross Ning <rossning92@gmail.com>
2018-09-07 01:21:37 -0700

    message

--- scripts/Ross/disable_security.cmd
+++ scripts/Ross/disable_security.cmd
@@ -4,7 +4,8 @@
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
 
 :: Disable UAC
-reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+:: reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f 
 
 :: Disable Firewall
 NetSh Advfirewall set allprofiles state off
