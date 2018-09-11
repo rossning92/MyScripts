d062159638820494c77748c79fa4d8d37065c29c
Ross Ning <rossning92@gmail.com>
2018-09-10 23:38:02 -0700

    message

--- scripts/Ross/install_PyTorch.cmd
+++ scripts/Ross/install_PyTorch.cmd
@@ -1,6 +1,8 @@
 @echo off
 call _conda.cmd
 
-conda install pytorch cuda92 -c pytorch
+call activate.bat
+
+conda install pytorch cuda92 -c pytorch -y
 pip3 install torchvision
 python -m pip install --upgrade pip
