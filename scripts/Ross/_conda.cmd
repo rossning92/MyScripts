6f6bd67c0530ff7a4a73fdbbb550f3f45d2bb29e
Ross Ning <rossning92@gmail.com>
2018-09-10 23:29:43 -0700

    message

--- scripts/Ross/_conda.cmd
+++ scripts/Ross/_conda.cmd
@@ -1,3 +1,11 @@
 if exist %USERPROFILE%\AppData\Local\Continuum\anaconda3 (
     set PATH=%USERPROFILE%\AppData\Local\Continuum\anaconda3\Scripts;%PATH%
+	exit /b 0
 )
+
+if exist C:\tools\miniconda3 (
+	set PATH=C:\tools\miniconda3\Scripts;%PATH%
+	exit /b 0
+)
+
+exit /b 1
--- scripts/Ross/dev/Miniconda.cmd
+++
@@ -1,4 +0,0 @@
-call C:\tools\miniconda3\Scripts\activate.bat
-
-cd /d "{{STARTUP_DIR}}"
-start
