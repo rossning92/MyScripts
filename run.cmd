530eeacc70e5c0932775539a58a0b0895a8353ff
Ross Ning <rossning92@gmail.com>
2018-09-12 12:07:33 +0800

    message

--- run.cmd
+++ run.cmd
@@ -9,13 +9,12 @@ set QML2_IMPORT_PATH=
 
 set CONDA_PATH=%LOCALAPPDATA%\Continuum\anaconda3
 
-set PATH=%CONDA_PATH%;%CONDA_PATH%\Scrtips;C:\Python36;C:\Python36\Scripts;%LOCALAPPDATA%\Programs\Python\Python36;%LOCALAPPDATA%\Programs\Python\Python36\Scripts;%PATH%
+set PATH=%CONDA_PATH%;%CONDA_PATH%\Scripts;C:\Python36;C:\Python36\Scripts;%LOCALAPPDATA%\Programs\Python\Python36;%LOCALAPPDATA%\Programs\Python\Python36\Scripts;%PATH%
 
+:: Install required packages
 where python
 if not %errorlevel%==0 (
 	call install\install.cmd
 )
-
-call install.cmd
 
 start "MyScripts - Console" python autoreload.py
