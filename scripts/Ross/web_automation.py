5dcd8e35c99dc34d7513142c82d2f649c925434a
Ross Ning <rossning92@gmail.com>
2018-10-29 12:01:03 +0800

    message

--- scripts/Ross/web_automation.py
+++ scripts/Ross/web_automation.py
@@ -1,5 +1,12 @@
+import os
+import sys
 from selenium import webdriver
 
+# sys.path.insert(0, r'E:\bin\selenium')
+
+chrome_profile = os.environ['LOCALAPPDATA'] + '\\Google\\Chrome\\User Data\\Default'
+chrome_profile = 'C:\\ChromeProfile'
+
 options = webdriver.ChromeOptions()
-options.add_argument("user-data-dir=C:\\Path") #Path to your chrome profile
-w = webdriver.Chrome(executable_path="C:\\Users\\chromedriver.exe", chrome_options=options)
+options.add_argument('user-data-dir=%s' % chrome_profile)  # Path to your chrome profile
+w = webdriver.Chrome(chrome_options=options)
