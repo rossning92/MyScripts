272ce1933835aacec7f1f1853d8b37e8da340146
Ross Ning <rossning92@gmail.com>
2018-10-31 10:53:45 -0700

    message

--- scripts/Ross/web_automation.py
+++ scripts/Ross/web_automation.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 # sys.path.insert(0, r'E:\bin\selenium')
 
 chrome_profile = os.environ['LOCALAPPDATA'] + '\\Google\\Chrome\\User Data\\Default'
+#print(chrome_profile)
 chrome_profile = 'C:\\ChromeProfile'
 
 options = webdriver.ChromeOptions()
