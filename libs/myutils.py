a9aa858c23fee9646b5644194bb35c225cfdab23
Ross Ning <rossning92@gmail.com>
2018-11-04 10:59:23 +0800

    message

--- libs/myutils.py
+++ libs/myutils.py
@@ -12,33 +12,6 @@ import sys
 ADD_PARENT_DIRS_TO_PYTHON_PATH = True
 
 
-def cb_get_file():
-    if sys.platform == 'win32':
-        import win32clipboard
-
-        file_path = None
-        win32clipboard.OpenClipboard(None)
-
-        fmt = 0
-        while True:
-            fmt = win32clipboard.EnumClipboardFormats(fmt)
-            if fmt == 0:
-                break
-
-            if fmt > 0xC000:
-                fmt_name = win32clipboard.GetClipboardFormatName(fmt)
-                # print(fmt_name)
-
-                if fmt_name == 'FileNameW':
-                    data = win32clipboard.GetClipboardData(fmt)
-                    file_path = data.decode('utf-16').strip('\0x00')
-
-        win32clipboard.CloseClipboard()
-        return file_path
-    else:
-        return None
-
-
 def open_text_editor(path):
     path = os.path.abspath(os.getcwd() + os.path.sep + path)
 
