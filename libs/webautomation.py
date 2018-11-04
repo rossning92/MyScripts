4ced2513d16e597f4233d584123538fbd60bd78c
Ross Ning <rossning92@gmail.com>
2018-11-03 19:08:46 -0700

    message

---
+++ libs/webautomation.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import re
+
+driver = None
+
+
+def click(ele):
+    driver.execute_script('arguments[0].scrollIntoView();', ele)
+    ActionChains(driver).move_to_element(ele).click().perform()
+
+
+def find_by_text(text):
+    return driver.find_element_by_xpath('//*[contains(text(), "%s")]' % text)
+
+
+def find_by_css(css):
+    return driver.find_element_by_css_selector(css)
+
+
+def send_keys(keys):
+    ActionChains(driver).send_keys(keys).perform()
+
+
+def init():
+    global driver
+    chrome_profile = 'C:\\ChromeProfile'
+    options = webdriver.ChromeOptions()
+    options.add_argument('user-data-dir=%s' % chrome_profile)  # Path to your chrome profile
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.implicitly_wait(10)
+
+
+init()
