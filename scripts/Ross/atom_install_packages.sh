dd24f224dd259d76058254e67755e262feca802a
Ross Ning <rossning92@gmail.com>
2018-11-17 14:29:37 +0800

    message

--- scripts/Ross/atom_install_packages.sh
+++ scripts/Ross/atom_install_packages.sh
@@ -6,6 +6,8 @@ apm install latex-autocomplete
 # Markdown
 apm install markdown-preview-enhanced
 apm install markdown-img-paste
+apm install markdown-writer
+#markdown-image-assistant
 
 apm install sublime-style-column-selection
 
