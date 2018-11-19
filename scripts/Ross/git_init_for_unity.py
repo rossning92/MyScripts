ab3c53f8a6d2899ac0eab5104aee1693aec17cd9
Ross Ning <rossning92@gmail.com>
2018-11-18 19:28:11 -0800

    message

--- scripts/Ross/git_init_for_unity.py
+++ scripts/Ross/git_init_for_unity.py
@@ -1,10 +1,10 @@
 import os
-import sys
 from urllib.request import urlretrieve
-
+from subprocess import call
 
 os.chdir(os.environ['CURRENT_FOLDER'])
 
 urlretrieve('https://raw.githubusercontent.com/github/gitignore/master/Unity.gitignore', '.gitignore')
 
-
+call('git init')
+call('git add -A')
