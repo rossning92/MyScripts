68f2167715b5f4f51ad6ac9cf2e6aecb086658ff
Ross Ning <rossning92@gmail.com>
2018-12-01 02:00:47 -0800

    message

--- scripts/ext/sync_scripts.sh
+++ scripts/ext/sync_scripts.sh
@@ -5,10 +5,10 @@ git config credential.helper store
 
 if [ ! -d ".git" ]; then
 	git init
-	git remote add origin https://github.com/rossning92/MyScripts
-	git branch --set-upstream-to=origin/master master
+	git remote add origin https://github.com/rossning92/MyScripts	
 	git fetch
 	git reset --hard origin/master
+	git branch --set-upstream-to=origin/master master
 fi
 
 git add -A
