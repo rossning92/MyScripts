613219720b6540542aff08dd66cc37c31e2afbbb
Ross Ning <rossning92@gmail.com>
2018-12-01 16:34:50 +0800

    message

--- scripts/ext/sync_scripts.sh
+++ scripts/ext/sync_scripts.sh
@@ -1,4 +1,14 @@
+set -e
+cd ../..
+
 git config credential.helper store
+
+if [ ! -d ".git" ]; then
+	git init
+	git remote add origin https://github.com/rossning92/MyScripts
+	git fetch
+	git pull
+fi
 
 git add -A
 git commit -m 'message'
