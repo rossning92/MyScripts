25dcc3bb817e0ab08b1c1186b59d46117b9bd539
Ross Ning <rossning92@gmail.com>
2018-12-10 09:19:30 +0800

    message

--- install/find_python.cmd
+++ install/find_python.cmd
@@ -1,16 +1,28 @@
 @echo off
 
 call :find_python %LOCALAPPDATA%\Continuum\anaconda3
-call :find_python %USERPROFILE%\anaconda3
-call :find_python C:\tools\miniconda3
-call :find_python %LOCALAPPDATA%\Programs\Python\Python36
-call :find_python C:\Python36
+if %errorlevel%==0 exit /b 0
 
-exit /b 0
+call :find_python %USERPROFILE%\anaconda3
+if %errorlevel%==0 exit /b 0
+
+call :find_python C:\tools\miniconda3
+if %errorlevel%==0 exit /b 0
+
+call :find_python %LOCALAPPDATA%\Programs\Python\Python36
+if %errorlevel%==0 exit /b 0
+
+call :find_python C:\Python36
+if %errorlevel%==0 exit /b 0
+
+exit /b 1
+
 
 :find_python 
 if exist "%~1" (
 	echo Python found: %~1
 	set "PATH=%~1;%~1\Scripts;%PATH%"
+	exit /b 0
+) else (
+	exit /b 1
 )
-exit /b 0
