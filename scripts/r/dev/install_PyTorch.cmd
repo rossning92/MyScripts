4908bd04dc51aca29513ff700dc92dcddc4266fd
Ross Ning <rossning92@gmail.com>
2019-01-05 22:41:11 -0800

    message

--- scripts/r/dev/install_PyTorch.cmd
+++ scripts/r/dev/install_PyTorch.cmd
@@ -1,7 +1,7 @@
 @echo off
-call conda\_conda.cmd
+set "PATH=C:\tools\miniconda3;C:\tools\miniconda3\Scripts;%PATH%"
 
-call activate.bat
+:: call activate.bat
 
 conda install pytorch cuda92 -c pytorch -y
 pip install torchvision
