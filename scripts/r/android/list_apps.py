c5505e105219c6267b91fdc011210f87e18af842
Ross Ning <rossning92@gmail.com>
2019-01-30 17:27:13 -0800

    message

--- scripts/r/android/list_apps.py
+++ scripts/r/android/list_apps.py
@@ -1,5 +1,25 @@
 from _shutil import *
 from _gui import *
 
-lines = check_output('adb shell pm list packages').decode().splitlines()
-search(lines)
+s = check_output('adb shell pm list packages').decode()
+s = s.replace('package:', '')
+lines = s.splitlines()
+lines = sorted(lines)
+i = search(lines)
+if i == -1:
+    sys.exit(1)
+
+pkg = lines[i]
+print("Selected: " + pkg)
+
+opt = [
+    'start',
+    'uninstall'
+]
+
+i = search(opt)
+if i == -1:
+    sys.exit(1)
+
+if opt[i] == 'start':
+    call('adb shell monkey -p %s -c android.intent.category.LAUNCHER 1' % pkg)
