7a1e0ccf4e9d18c790d05ee8bd9cf2b4512a1e1a
Ross Ning <rossning92@gmail.com>
2019-02-13 17:17:32 -0800

    message

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+pip3.6 install -r requirement.txt
 python3.6 autoreload.py
--- scripts/r/graphviz.py
+++ scripts/r/graphviz.py
@@ -1,7 +1,11 @@
 from _shutil import *
 
 chdir(r'{{GRAPHVIZ_SRC_FOLDER}}')
-src_file = list(glob.glob('*.dot'))[0]
+files = list(glob.glob('*.dot'))
+files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+src_file = files[0]
+
 dst_file = os.path.splitext(src_file)[0] + '.png'
 
-call(f'cmd /c dot -Tpng {src_file} -o {dst_file} & {dst_file}')
+call(f'cmd /c dot -Tpng "{src_file}" -o "{dst_file}"')
+Popen([r"C:\Program Files\IrfanView\i_view64.exe", dst_file])
