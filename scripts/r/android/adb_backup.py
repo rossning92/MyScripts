c91fc2e2ba9459e0bd95c8d7ca371fd5603f983b
Ross Ning <rossning92@gmail.com>
2019-03-24 10:43:01 +0800

    message

---
+++ scripts/r/android/adb_backup.py
@@ -0,0 +1,17 @@
+from _shutil import *
+
+APP = 'com.facebook.mobilehome'
+APP = 'com.coolapk.market'
+APP = 'cn.mdict'
+
+d = 'C:\\tools\\abe'
+mkdir(d)
+chdir(d)
+
+download('https://github.com/nelenkov/android-backup-extractor/releases/download/20181012025725-d750899/abe-all.jar')
+
+chdir(expanduser('~/Desktop'))
+
+call(f'adb backup -apk -f backup.ab {APP}')
+
+call(f'java -jar "{d}/abe-all.jar" unpack backup.ab backup.tar')
