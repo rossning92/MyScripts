0512704a1b075fcd176b4ce13f52e2e800a2ca82
Ross Ning <rossning92@gmail.com>
2019-03-24 12:49:21 +0800

    message

--- scripts/r/android/backup_apk.py
+++ scripts/r/android/backup_apk.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 from colorama import init
 from colorama import Fore, Back, Style
+import re
 
 init()
 
@@ -21,14 +22,21 @@ pkgs = [x.replace('package:', '') for x in lines]
 total = len(pkgs)
 for i in range(total):
     pkg_name = pkgs[i]
+    pkg_name = 'com.github.shadowsocks'
     print(Fore.LIGHTYELLOW_EX + '(%d / %d) Backup %s ...' % (i + 1, total, pkg_name) + Fore.RESET)
 
     if os.path.exists('%s.apk' % pkg_name):
         continue
 
     # Get apk path
+    # 'package:/data/app/com.github.uiautomator-1AfatTFmPxzjNwUtT-5h7w==/base.apk'
     out = subprocess.check_output('adb shell pm path %s' % pkg_name)
     apk_path = out.decode().strip().replace('package:', '')
 
-    # Pull
+    # Pull apk
     subprocess.call('adb pull %s %s.apk' % (apk_path, pkg_name))
+
+    # Pull data
+    # subprocess.call(f'adb shell su -c tar -cvf /sdcard/123.tar /data/data/{pkg_name}')
+    # subprocess.call(f'adb pull /sdcard/123.tar')
+    break
