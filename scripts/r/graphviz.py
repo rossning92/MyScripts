5f082f8ea5d1fcce2d7d80fef0ad12f5b08aeb45
Ross Ning <rossning92@gmail.com>
2019-04-02 20:00:37 -0700

    message

--- scripts/r/graphviz.py
+++ scripts/r/graphviz.py
@@ -5,8 +5,11 @@ files = list(glob.glob('*.dot'))
 files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
 src_file = files[0]
 
-fmt = 'png'
-dst_file = os.path.splitext(src_file)[0] + f'.{fmt}'
+formats = ['png', 'svg']
+for fmt in formats:
+    dst_file = os.path.splitext(src_file)[0] + f'.{fmt}'
 
-call(f'cmd /c dot -T{fmt} "{src_file}" -o "{dst_file}"')
-Popen([r"C:\Program Files\IrfanView\i_view64.exe", dst_file])
+    call(f'cmd /c dot -T{fmt} "{src_file}" -o "{dst_file}"')
+
+    if fmt == 'png':
+        Popen([r"C:\Program Files\IrfanView\i_view64.exe", dst_file])
