9c71f33a06a4fe40b6ae534232b01e5716d6381f
Ross Ning <rossning92@gmail.com>
2019-04-05 12:24:42 -0700

    message

--- scripts/r/grep_code.py
+++ scripts/r/grep_code.py
@@ -2,76 +2,74 @@ from _term import *
 from _shutil import *
 from _editor import *
 
-SRC_DIR = r'{{SOURCE_FOLDER}}'
 
-repo_root = None
-rel_path = None
+def grep(src_dir):
+    repo_root = None
+    rel_path = None
+    ps = None
+    lines = []
+    files = set()
+    cur_text = None
 
-if False:
-    tmp_path = ''
-    for comp in SRC_DIR.split('\\'):
-        tmp_path += comp + '\\'
-        if exists(tmp_path + '.gitignore'):
-            print('REPO ROOT: ' + tmp_path)
-            repo_root = tmp_path
-            rel_path = SRC_DIR.replace(repo_root, '')
-            print(rel_path)
-            break
+    if False:
+        tmp_path = ''
+        for comp in src_dir.split('\\'):
+            tmp_path += comp + '\\'
+            if exists(tmp_path + '.gitignore'):
+                print('REPO ROOT: ' + tmp_path)
+                repo_root = tmp_path
+                rel_path = src_dir.replace(repo_root, '')
+                print(rel_path)
+                break
 
-    input()
+        input()
 
+    def text_changed(s):
+        nonlocal ps, cur_text
+        cur_text = s
+        if ps:
+            ps.kill()
+            ps = None
 
-def text_changed(s):
-    global cur_text
-    global ps
+    def item_selected(i):
+        arr = lines[i].split(':')
+        file = os.path.join(src_dir, arr[0])
+        line_no = arr[1]
+        open_in_androidstudio(file, line_no)
 
-    cur_text = s
-    if ps:
-        ps.kill()
-        ps = None
+    if repo_root:
+        chdir(repo_root)
+    else:
+        chdir(src_dir)
+    lw = ListWidget(lines=lines, text_changed=text_changed, item_selected=item_selected)
 
+    text = None
+    while True:
+        while cur_text == text:
+            time.sleep(0.1)
+            lw.update()
+        text = cur_text
 
-def item_selected(i):
-    arr = lines[i].split(':')
-    file = os.path.join(SRC_DIR, arr[0])
-    line_no = arr[1]
-    open_in_androidstudio(file, line_no)
+        if text == '':
+            lines.clear()
+            continue
 
+        args = f'rg --line-number -F "{text}"'
+        if rel_path:
+            args += ' ' + rel_path
+        args += ' -g "!intermediates/" -g "!build/"'
 
-ps = None
-lines = []
-files = set()
-cur_text = None
-if repo_root:
-    chdir(repo_root)
-else:
-    chdir(SRC_DIR)
-lw = ListWidget(lines=lines, text_changed=text_changed, item_selected=item_selected)
+        ps = check_output2(args)
 
-text = None
-while True:
-    while cur_text == text:
-        time.sleep(0.1)
-        lw.update()
-    text = cur_text
-
-    if text == '':
         lines.clear()
-        continue
+        files.clear()
+        for l in ps.readlines(block=False, timeout=0.1):
+            if l is not None:
+                s = l.decode(errors='replace')
+                s = s.strip()
+                lines.append(s)
 
-    args = f'rg --line-number -F "{text}"'
-    if rel_path:
-        args += ' ' + rel_path
-    args += ' -g "!intermediates/" -g "!build/"'
+            lw.update()
 
-    ps = check_output2(args)
 
-    lines.clear()
-    files.clear()
-    for l in ps.readlines(block=False, timeout=0.1):
-        if l is not None:
-            s = l.decode(errors='replace')
-            s = s.strip()
-            lines.append(s)
-
-        lw.update()
+grep(src_dir=r'{{SOURCE_FOLDER}}')
