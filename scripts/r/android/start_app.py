a690e6769dc659813de83e7c341ccf89c59f7071
Ross Ning <rossning92@gmail.com>
2019-04-08 23:59:12 -0700

    message

---
+++ scripts/r/android/start_app.py
@@ -0,0 +1,11 @@
+from _shutil import *
+from _script import *
+from _android import *
+
+pkg = r'{{PKG_NAME}}'
+
+call(f'adb shell am force-stop {pkg}')
+
+start_app(pkg)
+
+run_script('logcat', variables={'PKG_NAME': pkg}, new_window=True)
