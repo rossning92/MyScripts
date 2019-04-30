887a5f9a44cd19885ad440b8fd0da08eea274b41
Ross Ning <rossning92@gmail.com>
2019-04-30 13:01:09 +0800

    message

--- scripts/r/HotkeyR.py
+++ scripts/r/HotkeyR.py
@@ -4,5 +4,8 @@ mkdir('~/Projects')
 chdir('~/Projects')
 
 if not exists('HotkeyR'):
+    print('Cloning HotkeyR...')
     call('git clone https://github.com/rossning92/HotkeyR')
+
+print('Starting HotkeyR...')
 Popen(['AutoHotkeyU64.exe', expanduser('HotkeyR/HotkeyR.ahk')])
