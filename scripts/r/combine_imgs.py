af5cdfa3ffd6db98468e8548ce0ef6e850d68e94
Ross Ning <rossning92@gmail.com>
2019-05-14 17:06:23 -0700

    message

--- scripts/r/combine_imgs.py
+++ scripts/r/combine_imgs.py
@@ -3,6 +3,9 @@ from _shutil import *
 
 os.chdir(env['CURRENT_FOLDER'])
 
+cols = '{{COMBINE_IMG_COLS}}'
+cols = int(cols) if cols else None
+
 combine_images(image_files='*.png',
                out_file='out.png',
-               cols={{COMBINE_IMG_COLS}})
+               cols=cols)
