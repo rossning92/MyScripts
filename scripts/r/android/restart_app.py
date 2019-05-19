7454791c68584265292cb4172afa762059b4d6b2
Ross Ning <rossning92@gmail.com>
2019-05-20 06:24:43 +0800

    message

--- scripts/r/android/restart_app.py
+++ scripts/r/android/restart_app.py
@@ -9,4 +9,4 @@ call(f'adb shell am force-stop {pkg}')
 start_app(pkg)
 
 if '{{START_APP_SHOW_LOGCAT}}':
-    run_script('logcat', variables={'PKG_NAME': pkg, 'LOGCAT_FILTER': ''})
+    logcat(r'{{PKG_NAME}}')
