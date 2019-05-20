db502802f064883def8926052c6e72540d66efb7
Ross Ning <rossning92@gmail.com>
2019-05-20 11:05:20 +0800

    message

--- scripts/r/UE4/install_RenderDoc.py
+++ scripts/r/UE4/install_RenderDoc.py
@@ -5,23 +5,36 @@ from _shutil import *
 
 os.chdir(os.path.join(os.environ['USERPROFILE'], 'Downloads'))
 
-while True:
-    try:
-        from requests_html import HTMLSession
 
-        break
-    except:
-        subprocess.call(['python', '-m', 'pip', 'install', 'requests_html'])
+def install_nightly():
+    while True:
+        try:
+            from requests_html import HTMLSession
 
-session = HTMLSession()
+            break
+        except:
+            subprocess.call(['python', '-m', 'pip', 'install', 'requests_html'])
 
-r = session.get('https://renderdoc.org/builds')
-nodes = r.html.xpath(".//a[contains(text(),'Nightly') and contains(text(),'ZIP')]")
-url = nodes[0].attrs['href']
+    session = HTMLSession()
 
-f = download(url)
-print(f)
+    r = session.get('https://renderdoc.org/builds')
+    nodes = r.html.xpath(".//a[contains(text(),'Nightly') and contains(text(),'ZIP')]")
+    url = nodes[0].attrs['href']
 
-DEST = r'C:\Tools\RenderDoc'
-remove(DEST)
-unzip(f, DEST)
+    f = download(url)
+    print(f)
+
+    DEST = r'C:\Tools\RenderDoc'
+    remove(DEST)
+    unzip(f, DEST)
+
+
+def install():
+    download('https://renderdoc.org/stable/1.4/RenderDoc_1.4_64.msi', 'RenderDoc_1.4_64.msi')
+    call('msiexec /i RenderDoc_1.4_64.msi /quiet /qn /norestart')
+
+
+if '{{RENDERDOC_INSTALL_NIGHTLY}}':
+    install_nightly()
+else:
+    install()
