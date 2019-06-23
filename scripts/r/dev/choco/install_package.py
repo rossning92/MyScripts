8aa678b1510715b3742a77535429e61fd9d4ff23
Ross Ning <rossning92@gmail.com>
2019-06-23 13:34:25 +0800

    message

--- scripts/r/dev/choco/install_package.py
+++ scripts/r/dev/choco/install_package.py
@@ -2,63 +2,56 @@ from _gui import search
 import sys
 import subprocess
 
-other = [
-    'miktex',
-    'unity --version 2018.2.0',
-    'unity --version 2018.2.14',
-    'sketchup',
-    'blender',
-    'graphviz',
-    'inkscape',
-    'mousewithoutborders',
-    'tightvnc',
-    'gifcam',
-    'autohotkey',
-    'nsis',
-]
+PKGS = {
+    'other': [
+        'miktex',
+        'unity --version 2018.2.0',
+        'unity --version 2018.2.14',
+        'sketchup',
+        'blender',
+        'graphviz',
+        'inkscape',
+        'mousewithoutborders',
+        'tightvnc',
+        'gifcam',
+        'autohotkey',
+        'nsis',
+    ],
+    'common': [
+        'everything',
+        'irfanview',
+    ],
+    'dev': [
+        'conemu',
+        'atom',
+        'graphviz',
+        'anaconda3',
+        'miniconda3',
+        'pycharm-community',
+        'cmake',
+        'visualstudio2017community',
+        'androidstudio',
+        'android-sdk',
+        'llvm',
+        'ripgrep',
+        'vscode',
+        'sumatrapdf',
+    ],
+    'media': [
+        'ffmpeg',
+        'vlc',
+        'imagemagick',
+    ],
+    'for_work': [
+        'p4v',
+        'selenium-chrome-driver',
+    ],
+    'ue4': [
+        'directx',
+    ]
+}
 
-common = [
-    'everything',
-    'irfanview',
-]
-
-dev = [
-    'conemu',
-    'atom',
-    'graphviz',
-    'anaconda3',
-    'miniconda3',
-    'pycharm-community',
-    'cmake',
-    'visualstudio2017community',
-    'androidstudio',
-    'android-sdk',
-    'llvm',
-    'ripgrep',
-    'vscode',
-    'sumatrapdf',
-]
-
-media = [
-    'ffmpeg',
-    'vlc',
-    'imagemagick',
-]
-
-for_work = [
-    'p4v',
-    'selenium-chrome-driver',
-]
-
-other_options = [
-    '@for work',
-]
-
-ue4 = [
-    'directx',
-]
-
-pkg_list = sorted(other_options + other + dev + media + ue4 + common)
+pkg_list = ['@for work'] + sorted([app for cate in PKGS.values() for app in cate])
 idx = search(pkg_list)
 if idx < 0:
     sys.exit(1)
@@ -66,7 +59,7 @@ if idx < 0:
 subprocess.call('choco source add --name=chocolatey --priority=-1 -s="https://chocolatey.org/api/v2/"')
 
 if pkg_list[idx] == '@for work':
-    for pkg in for_work + media + dev + common:
+    for pkg in [PKGS['for_work'] + PKGS['media'] + PKGS['dev'] + PKGS['common']]:
         subprocess.call('choco install %s -y' % pkg)
 else:
     subprocess.call('choco install %s -y' % pkg_list[idx])
