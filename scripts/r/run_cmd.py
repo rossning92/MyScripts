fc4964906406338ce9e8ca79ad233af8d94f6330
Ross Ning <rossning92@gmail.com>
2019-07-21 23:00:36 -0700

    message

--- scripts/r/run_cmd.py
+++ scripts/r/run_cmd.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from _cmake import setup_cmake
 
 try:
     import _setup_android_env as android
@@ -11,4 +12,7 @@ if 'CURRENT_FOLDER' in os.environ:
     os.chdir(os.environ['CURRENT_FOLDER'])
 else:
     os.chdir(os.path.expanduser('~'))
+
+setup_cmake()
+
 subprocess.call(['cmd'])
