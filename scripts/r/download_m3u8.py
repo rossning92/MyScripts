725ad227ec516c9932714cb44688fd0da1fdc1c4
Ross Ning <rossning92@gmail.com>
2019-07-29 22:14:02 -0700

    message

--- scripts/r/download_m3u8.py
+++ scripts/r/download_m3u8.py
@@ -6,4 +6,4 @@ if not url.endswith('.m3u8'):
 
 chdir(expanduser('~/Desktop'))
 
-call2('ffmpeg -i %s -c copy -bsf:a aac_adtstoasc output.mp4' % url)
+call2('ffmpeg -i %s -c copy -bsf:a aac_adtstoasc %s.mp4' % (url, get_cur_time_str()))
