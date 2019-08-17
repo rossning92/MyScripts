015f5bcb28437e7b2f93360b80115d2895ab6094
Ross Ning <rossning92@gmail.com>
2019-08-16 17:00:07 -0700

    message

--- scripts/r/open_jupyter_notebook.py
+++ scripts/r/open_jupyter_notebook.py
@@ -1,5 +1,7 @@
-import _conda; _conda.setup_env()
+from _conda import setup_env
 from _shutil import *
 import os
+
+setup_env()
 
 call(f'call activate & jupyter notebook "{os.environ["SELECTED_FILE"]}"')
