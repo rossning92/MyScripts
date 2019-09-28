97a6621e84fe70caa3d91196a32e6d037b941c4e
rossning92 <rossning92@github.com>
2019-09-28 00:37:33 -0700

    message

--- libs/_appmanager.py
+++ libs/_appmanager.py
@@ -4,16 +4,15 @@ from os.path import dirname, join
 from _shutil import run_elevated
 import subprocess
 import sys
+from _script import run_script
 
 
 def get_executable(app_name):
-    if sys.platform == 'linux':
-        return
-
     with open(join(dirname(__file__), 'app_list.yaml'), 'r') as f:
         app_list = yaml.load(f.read(), Loader=yaml.FullLoader)
 
-    matched_apps = [k for k, v in app_list.items() if app_name.lower() == k.lower()]
+    matched_apps = [k for k, v in app_list.items(
+    ) if app_name.lower() == k.lower()]
     app_name = matched_apps[0]
     app = app_list[app_name]
 
@@ -32,14 +31,21 @@ def get_executable(app_name):
     executable = find_executable()
 
     if executable is None:
-        run_elevated('choco source add --name=chocolatey --priority=-1 -s="https://chocolatey.org/api/v2/"')
+        if sys.platform == 'win32':
+            run_elevated(
+                'choco source add --name=chocolatey --priority=-1 -s="https://chocolatey.org/api/v2/"')
 
-        pkg_name = app_name
-        if 'choco' in app:
-            pkg_name = app['choco']
-        print('Installing %s...' % pkg_name)
-        run_elevated(['choco', 'install', pkg_name, '-y'])
-        executable = find_executable()
+            pkg_name = app_name
+            if 'choco' in app:
+                pkg_name = app['choco']
+            print('Installing %s...' % pkg_name)
+            run_elevated(['choco', 'install', pkg_name, '-y'])
+            executable = find_executable()
+
+        elif sys.platform == 'linux':
+            if 'linux_install' in app:
+                run_script(app['linux_install'])
+                executable = find_executable()
 
     return executable
 
