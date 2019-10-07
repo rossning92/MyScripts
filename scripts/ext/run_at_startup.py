0df4347a605f5e0368400db770a7db2091cd5f69
Ross Ning <rossning92@gmail.com>
2019-10-07 00:27:18 -0700

    message

--- scripts/ext/run_at_startup.py
+++ scripts/ext/run_at_startup.py
@@ -3,5 +3,5 @@ from _shutil import *
 if sys.platform == 'win32':
     path = os.path.realpath('../../run.cmd')
 
-    call2('reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v MyScripts /t REG_SZ /d """' +
-          path + '""" /f')
+    call2('reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v MyScripts /t REG_SZ /d "\\"' +
+          path + '\\"" /f')
