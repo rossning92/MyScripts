ff1e3518a262d0496c470ad70672d7d48bfdbf43
rossning92 <rossning92@github.com>
2019-10-28 00:18:54 -0700

    message

--- scripts/r/uiautomator_example.py
+++ scripts/r/uiautomator_example.py
@@ -4,7 +4,8 @@ from _android import *
 setup_android_env()
 
 # https://github.com/xiaocong/uiautomator
+# "C:\Android\android-sdk\tools\bin\uiautomatorviewer.bat"
 
-call('pip install uiautomator')
+call('pip install uiautomator --upgrade')
 
 call('python -c "import code; from uiautomator import device as d; code.interact(local=locals())"')
