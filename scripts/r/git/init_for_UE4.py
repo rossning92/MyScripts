5d464df1d59d7863d7d650d7e2665de203506e86
Ross Ning <rossning92@gmail.com>
2019-11-01 16:25:34 -0700

    message

--- scripts/r/git/init_for_UE4.py
+++ scripts/r/git/init_for_UE4.py
@@ -1,11 +1,16 @@
 import os
 from urllib.request import urlretrieve
 from subprocess import call
+from _script import *
 
-os.chdir(os.environ['CURRENT_FOLDER'])
+proj_path = os.path.realpath(os.environ['CURRENT_FOLDER'])
+
+os.chdir(proj_path)
 
 urlretrieve('https://raw.githubusercontent.com/github/gitignore/master/UnrealEngine.gitignore', '.gitignore')
 
 call('git init')
 call('git add -A')
 call('git commit -m "initial commit"')
+
+set_variable('GIT_REPO', proj_path)
