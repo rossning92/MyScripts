e39b3905c8784277202b3755ecb2ad9bfc6c4f40
rossning92 <rossning92@github.com>
2019-11-14 22:44:55 -0800

    message

--- scripts/r/customize_windows.cmd
+++ scripts/r/customize_windows.cmd
@@ -42,5 +42,30 @@ call disable_UAC.cmd
 :: Turn off animation
 reg add "HKCU\Control Panel\Desktop\WindowMetrics" /v MinAnimate /t REG_DWORD /d 0 /f
 
+
+:: Customize console window
+reg add HKCU\Console /v CodePage /t REG_DWORD /d 65001 /f
+reg add HKCU\Console /v FaceName /t REG_SZ /d Consolas /f
+reg add HKCU\Console /v FontSize /t REG_DWORD /d 0x120000 /f
+
+reg add HKCU\Console /v ColorTable00 /t REG_DWORD /d 0x00332c27 /f
+reg add HKCU\Console /v ColorTable01 /t REG_DWORD /d 0x00efaf61 /f
+reg add HKCU\Console /v ColorTable02 /t REG_DWORD /d 0x0079c398 /f
+reg add HKCU\Console /v ColorTable03 /t REG_DWORD /d 0x00c1b556 /f
+reg add HKCU\Console /v ColorTable04 /t REG_DWORD /d 0x00756cdf /f
+reg add HKCU\Console /v ColorTable05 /t REG_DWORD /d 0x00dd77c5 /f
+reg add HKCU\Console /v ColorTable06 /t REG_DWORD /d 0x007ac0e4 /f
+reg add HKCU\Console /v ColorTable07 /t REG_DWORD /d 0x00e4dfdb /f
+reg add HKCU\Console /v ColorTable08 /t REG_DWORD /d 0x0074635a /f
+reg add HKCU\Console /v ColorTable09 /t REG_DWORD /d 0x00efaf61 /f
+reg add HKCU\Console /v ColorTable10 /t REG_DWORD /d 0x0079c398 /f
+reg add HKCU\Console /v ColorTable11 /t REG_DWORD /d 0x00c1b556 /f
+reg add HKCU\Console /v ColorTable12 /t REG_DWORD /d 0x00756cdf /f
+reg add HKCU\Console /v ColorTable13 /t REG_DWORD /d 0x00dd77c5 /f
+reg add HKCU\Console /v ColorTable14 /t REG_DWORD /d 0x007ac0e4 /f
+reg add HKCU\Console /v ColorTable15 /t REG_DWORD /d 0x00e4dfdb /f
+
+reg add HKCU\Console\%%SystemRoot%%_SYSTEM32_cmd.exe /v CodePage /t REG_DWORD /d 65001 /f
+
 taskkill /f /im explorer.exe
 start explorer.exe
