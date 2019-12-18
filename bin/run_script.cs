8e5de688e04f395b32010ad924cf69eaa6c20fa6
Ross Ning <rossning92@gmail.com>
2019-12-17 21:14:48 -0800

    message

--- bin/run_script.cs
+++ bin/run_script.cs
@@ -4,19 +4,28 @@ using System.Diagnostics;
 
 class Program {
 
-    static void Main (string[] args) {
+    static string WrapArgument (string arg) {
+        if (arg.Contains (" ")) {
+            return "\"" + arg + "\"";
+        } else {
+            return arg;
+        }
+    }
+
+    static int Main (string[] args) {
         List<string> arguments = new List<string> ();
 
         string pythonFile = System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "run_script.py");
-        arguments.Add (pythonFile);
+        arguments.Add (WrapArgument (pythonFile));
 
         for (int i = 0; i < args.Length; i++) {
-            arguments.Add (args[i]);
+            arguments.Add (WrapArgument (args[i]));
         }
 
         var info = new ProcessStartInfo ("python", string.Join (" ", arguments));
         info.UseShellExecute = false;
         var proc = Process.Start (info);
         proc.WaitForExit ();
+        return proc.ExitCode;
     }
 }
