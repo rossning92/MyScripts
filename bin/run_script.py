c619cc9cc1c82a854f3f7f995e5393ee3f35b454
rossning92 <rossning92@github.com>
2019-12-25 12:46:30 -0800

    message

--- bin/run_script.py
+++ bin/run_script.py
@@ -2,16 +2,17 @@ import argparse
 import os
 import sys
 
-if True:
-    sys.path.insert(0, os.path.realpath(
-        os.path.dirname(__file__) + '/../libs'))
-    from _script import *
+sys.path.insert(0, os.path.realpath(
+    os.path.dirname(__file__) + '/../libs'))
+from _script import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--console_title', default=None)
 parser.add_argument('--restart_instance', type=bool, default=False)
 parser.add_argument('script_name', type=str)
 args = parser.parse_args()
+
+update_env_var_explorer()
 
 run_script(args.script_name,
            console_title=args.console_title,
