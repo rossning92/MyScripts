4aca56165e6839720816a0d624ee5b8fac1a4bf6
Ross Ning <rossning92@gmail.com>
2020-01-05 11:45:15 +0800

    message

--- scripts/r/web/timecut.py
+++ scripts/r/web/timecut.py
@@ -1,7 +1,23 @@
 from _shutil import *
 
+FPS = 60
+
 setup_nodejs()
+input_file = get_files(cd=True)[0]
 
-f = get_files(cd=True)[0]
+# https://github.com/tungs/timesnap
 
-call2(f'timecut {f} --viewport=1920,1080 --fps=25 --duration=1 --frame-cache --pix-fmt=yuv420p --output=video.mp4')
+# call2(f'timecut {f} --pipe-mode'
+#       f' --viewport=1920,1080'
+#       f' --fps=25 --duration=1'
+#       f' --frame-cache'
+#       f' --pix-fmt=yuv420p'
+#       f' --output=video.mp4')
+
+
+call2(f'timesnap {input_file}'
+      f' --viewport=1920,1080'
+      f' --fps={FPS}'
+      f' --duration=1'
+      f' --output-stdout'
+      f' | ffmpeg -framerate {FPS} -i pipe:0 -y video.mp4')
