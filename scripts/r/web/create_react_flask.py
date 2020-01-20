cf4a55bc7710bf27f641295c64acfeb1f29f2230
rossning92 <rossning92@github.com>
2020-01-20 11:21:16 -0800

    message

--- scripts/r/web/create_react_flask.py
+++ scripts/r/web/create_react_flask.py
@@ -4,7 +4,6 @@ import json
 
 setup_nodejs(install=True)
 get_executable('yarn')
-call2('pip install flask')
 
 cd(r'~/Projects')
 
@@ -21,12 +20,15 @@ with open('package.json', 'r') as f:
     data = json.load(f)
 
 # Backend framework: Flask
-data['proxy'] = 'http://localhost:5000'
-data['scripts']['server'] = 'set FLASK_APP=server.py&set FLASK_ENV=development&flask run'
-data['scripts']['dev'] = 'concurrently --kill-others-on-fail "yarn server" "yarn start"'
+if 'server' not in data['scripts']:
+    data['proxy'] = 'http://localhost:5000'
+    data['scripts']['server'] = 'set FLASK_APP=server.py&set FLASK_ENV=development&python -m flask run'
 
-with open('package.json', 'w') as f:
-    json.dump(data, f, indent=2)
+    call2('yarn add concurrently')  # Promise based AJAX library
+    data['scripts']['dev'] = 'concurrently --kill-others-on-fail "yarn server" "yarn start"'
+
+    with open('package.json', 'w') as f:
+        json.dump(data, f, indent=2)
 
 FLASK_APP = '''
 from flask import Flask
@@ -37,6 +39,7 @@ def hello_world():
     return 'Hello, World!'
 '''
 if not os.path.exists('server.py'):
+    call2('pip install flask')
     with open('server.py', 'w', newline='\n') as f:
         f.write(FLASK_APP)
 
