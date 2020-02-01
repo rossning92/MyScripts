37e297241fd762908dd50ec341248a18d2ead4aa
Ross Ning <rossning92@gmail.com>
2020-01-31 19:33:06 -0800

    message

--- templates/python.py
+++ templates/python.py
@@ -6,8 +6,10 @@ A simple python script template.
 
 from __future__ import absolute_import
 from __future__ import division
+from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import with_statement
+from io import open
 import argparse
 import os
 import sys
