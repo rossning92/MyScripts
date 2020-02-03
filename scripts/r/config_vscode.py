193a52b0bcc8e901df5ddb409e7628aed4bc0689
Ross Ning <rossning92@gmail.com>
2020-02-02 21:25:29 -0800

    message

--- scripts/r/config_vscode.py
+++ scripts/r/config_vscode.py
@@ -1,6 +1,25 @@
 from _shutil import *
 import json
 
+
+print2('Update key bindings...')
+with open(expandvars('%APPDATA%/Code/User/keybindings.json'), 'w') as f:
+    f.write('''
+[
+    {
+        "key": "ctrl+shift+v",
+        "command": "markdown-preview-enhanced.openPreviewToTheSide",
+        "when": "editorLangId == 'markdown'"
+    },
+    {
+        "key": "shift+alt+r",
+        "command": "revealFileInOS",
+    }
+]
+    '''.strip())
+
+
+print2('Install extensions...')
 extensions = '''
 donjayamanne.githistory
 ms-python.python
@@ -14,21 +33,9 @@ mdickin.markdown-shortcuts
 extensions = [x.strip() for x in extensions.splitlines()]
 extensions = [x for x in extensions if x]
 
-print2('Install extensions...')
 for ext in extensions:
     call_echo('code --install-extension %s' % ext)
 
-print2('Update key bindings...')
-with open(expandvars('%APPDATA%/Code/User/keybindings.json'), 'w') as f:
-    f.write('''
-[
-    {
-        "key": "ctrl+shift+v",
-        "command": "markdown-preview-enhanced.openPreviewToTheSide",
-        "when": "editorLangId == 'markdown'"
-    }
-]
-    '''.strip())
 
 print2('Update user settings...')
 setting_file = expandvars('%APPDATA%/Code/User/settings.json')
