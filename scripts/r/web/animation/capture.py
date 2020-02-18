06549c79f08e57afecb3b755d071792c2420dcf6
rossning92 <rossning92@github.com>
2020-02-17 23:04:47 -0800

    message

--- scripts/r/web/animation/capture.py
+++ scripts/r/web/animation/capture.py
@@ -4,20 +4,31 @@ import time
 import sys
 from _shutil import *
 
+# Fix for `callFunctionOn: Target closed.`
+# pip3 install websockets==6.0 --force-reinstall
+
+name = '{{_NAME}}'
+
 if True:
     sys.path.append('../../video')
     from ccapture_to_mov import convert_to_mov
 
 
 async def main():
-    browser = await launch(headless=False)
+    browser = await launch(headless=False,
+                           args=['--disable-dev-shm-usage'])
     page = await browser.newPage()
-    await page.goto('http://localhost:8080')
-    await page.screenshot({'path': 'example.png'})
+    url = 'http://localhost:8080'
+    if name:
+        url += '/%s.html' % name
+    await page.goto(url)
 
+    time.sleep(1.0)
+
+    print('Start capture.')
     await page.evaluate('() => { window.startCapture(); }')
 
-    await page.waitForXPath("//*[@id='captureStatus' and contains(., 'stopped')]")
+    await page.waitForXPath("//*[@id='captureStatus' and contains(., 'stopped')]", timeout=0)
 
     # Wait until file is saved
     time.sleep(2)
