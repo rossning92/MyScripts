7fffdaa2b7ab5d7c73ce69120ddf4a19ce11c6f0
rossning92 <rossning92@github.com>
2020-03-01 22:50:28 -0800

    message

--- scripts/r/web/animation/start_webpack_server.py
+++ scripts/r/web/animation/start_webpack_server.py
@@ -2,6 +2,11 @@ from _shutil import *
 
 cd('_threejs')
 
+setup_nodejs()
+
+if not os.path.exists('node_modules'):
+    call_echo('yarn install')
+
 dev_server = os.path.join('node_modules', '.bin', 'webpack-dev-server')
 args = r'%s --env.entryFolder="{{ANIMATION_PROJECT_PATH}}" --open --watch' % dev_server
 call_echo(args)
