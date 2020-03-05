5c61dcfc6e09529f4e0026a2107c2ec2e3923d8e
Ross Ning <rossning92@gmail.com>
2020-03-04 16:43:01 -0800

    message

---
+++ scripts/ext/run_script_in_remote_shell.py
@@ -0,0 +1,47 @@
+from _script import *
+
+if __name__ == '__main__':
+    script_path = os.environ['ROSS_SELECTED_SCRIPT_PATH']
+
+    script = ScriptItem(script_path)
+    s = script.render() + '\n'
+    tmp_script_file = write_temp_file(s, '.sh')
+
+    if script.ext != '.sh':
+        print('Script type is not supported: %s' % script.ext)
+        exit(0)
+
+    s = "cat > /tmp/script.sh <<'__EOF__'\n" + \
+        s + '\n__EOF__\nbash /tmp/script.sh\n'
+
+    # s = ''.join(['\\x%x' % ord(x) for x in s])
+    # s = "echo -n -e '" + s + "' > /tmp/tmpscript.sh && bash /tmp/tmpscript.sh"
+
+    set_clip(s)
+    # exec_ahk('''
+    #     WinActivate ahk_exe putty.exe
+    #     WinWaitActive ahk_exe putty.exe
+    #     if ErrorLevel
+    #         return
+
+    #     Send +{Ins}
+    # ''')
+
+    exec_ahk('''
+        WinActivate r/linux/et
+        WinWaitActive r/linux/et,, 2
+        if ErrorLevel
+            return
+
+        Send ^v
+
+        ; Loop 2 {
+        ;    WinWaitActive, ahk_exe ConEmu64.exe, Continue?, 2
+        ;    if ErrorLevel
+        ;        return
+        ;    Send {Enter}
+        ; }
+
+        ; Execute the last line
+        ; Send {Enter}
+    ''')
