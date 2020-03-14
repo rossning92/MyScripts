10f3e15266366698a756c195ee33ed272af1a2fc
Ross Ning <rossning92@gmail.com>
2020-03-13 19:22:13 -0700

    message

--- scripts/r/linux/et.sh
+++ scripts/r/linux/et.sh
@@ -4,5 +4,9 @@ if ! [ -x "$(command -v et)" ]; then
     sudo apt-get install et -y
 fi
 
+if ! [ -x "$(command -v expect)" ]; then
+    sudo apt-get install expect -y
+fi
+
 # et -x -r 15037:5037 {{SSH_USER}}@{{SSH_HOST}}:8080
 expect -c 'spawn et -x -r 15037:5037 -t 1234:22 {{SSH_USER}}@{{SSH_HOST}}:8080; expect "password:"; send "{{SSH_PWD}}\r"; interact;'
