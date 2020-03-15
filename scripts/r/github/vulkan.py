6d23d7bd7b12245cdc81dfaeca1764cc4fd8a4ca
rossning92 <rossning92@github.com>
2020-03-15 13:54:31 -0700

    message

---
+++ scripts/r/github/vulkan.py
@@ -0,0 +1,39 @@
+from _shutil import *
+from _cmake import *
+
+
+def f2():
+    if not exists('Vulkan'):
+        call2('git clone --recursive https://github.com/SaschaWillems/Vulkan.git')
+
+    chdir('Vulkan')
+
+    if not exists('vulkan_asset_pack.zip'):
+        call2('python download_assets.py')
+
+    setup_cmake()
+    call2('cmake -G "Visual Studio 15 2017 Win64"')
+    call2('start vulkanExamples.sln')
+
+
+setup_cmake()
+mkdir('C:\\Projects')
+chdir('C:\\Projects')
+
+# shutil.rmtree('VulkanSamples')
+
+if not exists('VulkanSamples'):
+    call2('git clone https://github.com/LunarG/VulkanSamples.git')
+
+chdir('VulkanSamples')
+mkdir('build')
+chdir('build')
+
+# Reference: https://github.com/LunarG/VulkanSamples/blob/master/BUILD.md
+if not exists('glslang'):
+    call2('python ../scripts/update_deps.py --arch x64')  # download and build dependencies
+
+print2('# Create the Visual Studio Project Files')
+call2('cmake -C helper.cmake -A x64 ..')
+
+subprocess.call('explorer .')
