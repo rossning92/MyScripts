1938374d169286321d31b647008d686b14ecb643
rossning92 <rossning92@github.com>
2020-03-18 20:19:16 -0700

    message

--- scripts/r/web/animation/start_webpack_server.py
+++ scripts/r/web/animation/start_webpack_server.py
@@ -1,5 +1,6 @@
 from _shutil import *
 
+sample_project_path = os.path.abspath('./_sample_project')
 
 setup_nodejs()
 project_path = r'{{ANIMATION_PROJECT_PATH}}' if r'{{ANIMATION_PROJECT_PATH}}' else None
@@ -11,6 +12,9 @@ if not os.path.exists('node_modules'):
 
 cd(project_path)
 call_echo('yarn link yo')
+
+copy(sample_project_path + '/', project_path + '/')
+
 os.environ['ENTRY_FOLDER'] = project_path
 script = os.path.join('node_modules', 'yo', 'bin', 'start-app.js')
 call_echo(['node', script])
