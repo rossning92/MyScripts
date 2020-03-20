36a959a87e3958d6ec672c596e3b0db739944a04
Ross Ning <rossning92@gmail.com>
2020-03-20 16:34:56 -0700

    message

--- scripts/r/md_to_pdf.py
+++ scripts/r/md_to_pdf.py
@@ -3,17 +3,21 @@ from r.open_with.open_with_ import open_with
 
 setup_nodejs()
 
-if r'{{MD_FILE}}':
-    f = r'{{MD_FILE}}'
-else:
-    f = get_files()[0]
+
+def convert_md_to_pdf(f):
+    try:
+        call2('mdpdf --version')
+    except:
+        call_echo('yarn global add mdpdf')
+
+    call_echo('mdpdf --format=A5 --border-top=1mm --border-bottom=1mm "%s"' % f)
+
+    open_with(os.path.splitext(f)[0] + '.pdf')
 
 
-try:
-    call2('mdpdf --version')
-except:
-    call_echo('yarn global add mdpdf')
-
-call_echo('mdpdf "%s" --format=A5' % f)
-
-open_with(os.path.splitext(f)[0] + '.pdf')
+if __name__ == '__main__':
+    if r'{{MD_FILE}}':
+        f = r'{{MD_FILE}}'
+    else:
+        f = get_files()[0]
+    convert_md_to_pdf(f)
