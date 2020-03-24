3f28138a8ebc89ddfeeff712eed6b7e47f7106aa
Ross Ning <rossning92@gmail.com>
2020-03-23 23:27:25 -0700

    message

--- scripts/r/md_to_pdf.py
+++ scripts/r/md_to_pdf.py
@@ -4,13 +4,14 @@ from r.open_with.open_with_ import open_with
 setup_nodejs()
 
 
-def convert_md_to_pdf(f):
+def convert_md_to_pdf(f, page_size='A5'):
     try:
         call2('mdpdf --version')
     except:
         call_echo('yarn global add mdpdf')
 
-    call_echo('mdpdf --format=A5 --border-top=1mm --border-bottom=1mm "%s"' % f)
+    call_echo(
+        f'mdpdf --format={page_size} --border-top=1mm --border-bottom=1mm "{f}"')
 
     open_with(os.path.splitext(f)[0] + '.pdf')
 
