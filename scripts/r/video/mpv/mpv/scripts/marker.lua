17c0d7c1f9ade940d22c43c4b29f902143934975
rossning92 <rossning92@github.com>
2020-03-29 16:22:40 -0700

    message

--- scripts/r/video/mpv/mpv/scripts/marker.lua
+++ scripts/r/video/mpv/mpv/scripts/marker.lua
@@ -2,10 +2,18 @@ local utils = require('mp.utils')
 
 local out = ''
 
+local function set_clipboard(text)
+    mp.commandv('run', 'powershell', 'set-clipboard', text)
+end
+
 local function add_marker()
     local time_pos = mp.get_property_number('time-pos')
     local time_pos_osd = mp.get_property_osd('time-pos/full')
-    mp.osd_message('Marker: ' .. time_pos_osd, 1)
+
+    local mouse_x, mouse_y = mp.get_mouse_pos()
+    local s = time_pos .. '!' .. mouse_x .. '!' .. mouse_y
+    set_clipboard(s)
+    mp.osd_message(s, 1)
 
     out = out .. time_pos .. '\n'
 end
@@ -18,7 +26,6 @@ local function save_to_text_file()
     file:write(out)
     file:close()
     mp.osd_message('Saved to: ' .. out_path, 3)
-
     out = ''
 end
 
