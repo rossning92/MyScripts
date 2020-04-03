276f08ae0fca9050d82af16dffb0277be255c36e
Ross Ning <rossning92@gmail.com>
2020-04-03 11:18:25 -0700

    message

--- scripts/r/android/screen_record.py
+++ scripts/r/android/screen_record.py
@@ -1,29 +1,29 @@
 from _shutil import *
 import signal
 
-max_secs = '{{_MAX_SECS}}'
-if not max_secs:
-    max_secs = 10
-else:
-    max_secs = int(max_secs)
 
-bit_rate = '{{_BIT_RATE}}' if '{{_BIT_RATE}}' else '20M'
+def screen_cap(max_secs=10, bit_rate='20M'):
+    signal.signal(signal.SIGINT, lambda a, b: None)
 
-signal.signal(signal.SIGINT, lambda a, b: None)
+    print('Press Ctrl-C to stop recording...')
 
-chdir('~/Desktop')
+    file = 'Recording_%s.mp4' % get_cur_time_str()
+    extra_args = f'--time-limit {max_secs} --bit-rate {bit_rate}'
 
-# call('adb shell "[ -e /sdcard/recording.mp4]; rm /sdcard/recording.mp4"')
+    if '{{_SIZE}}':
+        extra_args += ' --size {{_SIZE}}'
 
-print('Press Ctrl-C to stop recording...')
+    call(
+        f'adb shell screenrecord /sdcard/{file} {extra_args}', check_call=False)
 
-file = 'Recording_%s.mp4' % get_cur_time_str()
-extra_args = f'--time-limit {max_secs} --bit-rate {bit_rate}'
+    call(f'adb pull /sdcard/{file}')
+    call(f'cmd /c start {file}')
 
-if '{{_SIZE}}':
-    extra_args += ' --size {{_SIZE}}'
 
-call(f'adb shell screenrecord /sdcard/{file} {extra_args}', check_call=False)
+if __name__ == '__main__':
+    chdir('~/Desktop')
 
-call(f'adb pull /sdcard/{file}')
-call(f'cmd /c start {file}')
+    screen_cap(
+        max_secs=int('{{_MAX_SECS}}') if '{{_MAX_SECS}}' else 10,
+        bit_rate='{{_BIT_RATE}}' if '{{_BIT_RATE}}' else '20M'
+    )
