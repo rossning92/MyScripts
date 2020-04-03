92f95b2a08f5af1439cd17ea01fb9cd612970d81
Ross Ning <rossning92@gmail.com>
2020-04-03 11:27:15 -0700

    message

--- scripts/r/android/screen_record.py
+++ scripts/r/android/screen_record.py
@@ -2,28 +2,33 @@ from _shutil import *
 import signal
 
 
-def screen_cap(max_secs=10, bit_rate='20M'):
-    signal.signal(signal.SIGINT, lambda a, b: None)
-
+def screen_cap(out_file=None, max_secs=10, bit_rate='20M'):
     print('Press Ctrl-C to stop recording...')
 
-    file = 'Recording_%s.mp4' % get_cur_time_str()
+    signal.signal(signal.SIGINT, lambda a, b: None)
+
+    if out_file is None:
+        out_file = 'Recording_%s.mp4' % get_cur_time_str()
+
     extra_args = f'--time-limit {max_secs} --bit-rate {bit_rate}'
 
     if '{{_SIZE}}':
         extra_args += ' --size {{_SIZE}}'
 
     call(
-        f'adb shell screenrecord /sdcard/{file} {extra_args}', check_call=False)
+        f'adb shell screenrecord /sdcard/screen_record.mp4 {extra_args}', check_call=False)
 
-    call(f'adb pull /sdcard/{file}')
-    call(f'cmd /c start {file}')
+    call(f'adb pull /sdcard/screen_record.mp4 {out_file}')
+
+    return out_file
 
 
 if __name__ == '__main__':
     chdir('~/Desktop')
 
-    screen_cap(
+    out_file = screen_cap(
         max_secs=int('{{_MAX_SECS}}') if '{{_MAX_SECS}}' else 10,
         bit_rate='{{_BIT_RATE}}' if '{{_BIT_RATE}}' else '20M'
     )
+
+    call(f'cmd /c start {out_file}')
