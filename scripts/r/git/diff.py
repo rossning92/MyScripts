3959dcba9b0efabbdaf57079d08d5bfefb63c80f
rossning92 <rossning92@github.com>
2020-04-02 21:20:51 -0700

    message

--- scripts/r/git/diff.py
+++ scripts/r/git/diff.py
@@ -1,17 +1,21 @@
 from _shutil import *
 
-prepend_to_path([
-    r'C:\Program Files (x86)\Meld',
-    r'C:\Program Files (x86)\Meld\lib'
-])
+if '{{_USE_GUI}}':
+    prepend_to_path([
+        r'C:\Program Files (x86)\Meld',
+        r'C:\Program Files (x86)\Meld\lib'
+    ])
 
-call('git config --global diff.tool meld')
-call('git config --global difftool.prompt false')
-call(['git', 'config', '--global', 'difftool.meld.cmd', 'meld "$LOCAL" "$REMOTE"'])
+    call('git config --global diff.tool meld')
+    call('git config --global difftool.prompt false')
+    call(['git', 'config', '--global', 'difftool.meld.cmd', 'meld "$LOCAL" "$REMOTE"'])
 
 chdir(r'{{GIT_REPO}}')
 
-args = 'git difftool'
+
+args = 'git difftool' if '{{_USE_GUI}}' else 'git --no-pager diff'
+
+
 if '{{GIT_DIFF_PREVIOUS}}':
     args += ' HEAD~{{GIT_DIFF_PREVIOUS}}'
 
