811c8d7ad723c624c0a57ccf539383d9eebae55f
Ross Ning <rossning92@gmail.com>
2020-04-08 17:28:11 -0700

    message

--- scripts/r/unzip.py
+++ scripts/r/unzip.py
@@ -1,14 +1,22 @@
 from _appmanager import *
 from _shutil import *
 
-_7z = get_executable('7z')
-zip_file = get_files(cd=True)[0]
-out_folder = os.path.splitext(zip_file)[0]
-args = [
-    _7z,
-    'x',  # Extract
-    '-aoa',  # Overwrite all existing files
-    '-o' + out_folder,  # Out folder
-    zip_file,
-]
-call2(args)
+file = get_files(cd=True)[0]
+
+if file.endswith('.tar.gz'):
+    output_dir = file.rstrip('.tar.gz')
+    mkdir(output_dir)
+    call2('tar xzvf "%s" -C "%s"' % (file, output_dir))
+
+else:
+    _7z = get_executable('7z')
+
+    out_folder = os.path.splitext(file)[0]
+    args = [
+        _7z,
+        'x',  # Extract
+        '-aoa',  # Overwrite all existing files
+        '-o' + out_folder,  # Out folder
+        file,
+    ]
+    call2(args)
