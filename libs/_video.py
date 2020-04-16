2ccffcd4df46461292ef058686103a3338545262
Ross Ning <rossning92@gmail.com>
2020-04-15 18:03:28 -0700

    message

--- libs/_video.py
+++ libs/_video.py
@@ -4,48 +4,56 @@ import shlex
 import sys
 
 
-def generate_video_matrix(vid_files, titles=None, out_file=None, columns=None, fps=None, crop_rect=None):
-    os.environ['IMAGEMAGICK_BINARY'] = r"C:\Program Files\ImageMagick-7.0.9-Q16\magick.exe"
+def generate_video_matrix(
+    vid_files, titles=None, out_file=None, columns=None, fps=None, crop_rect=None
+):
+    os.environ[
+        "IMAGEMAGICK_BINARY"
+    ] = r"C:\Program Files\ImageMagick-7.0.9-Q16\magick.exe"
     try:
         import moviepy
     except:
-        subprocess.call('pip install moviepy')
+        subprocess.call("pip install moviepy")
 
     from moviepy.editor import VideoFileClip, TextClip, ColorClip, clips_array, vfx
     import numpy as np
     from moviepy.video.fx.all import crop
 
     if out_file is None:
-        out_file = 'combined.mp4'
+        out_file = "combined.mp4"
 
     if type(vid_files[0]) == str:
-        vid_clips = [VideoFileClip(
-            x, resize_algorithm='fast_bilinear') for x in vid_files]
+        vid_clips = [
+            VideoFileClip(x, resize_algorithm="fast_bilinear") for x in vid_files
+        ]
     else:
         vid_clips = vid_files
     max_h = np.max([x.h for x in vid_clips])
 
     vid_clips = [x.fx(vfx.resize, max_h / x.h) for x in vid_clips]
     if crop_rect:
-        vid_clips = [crop(x,
-                          x1=crop_rect[0],
-                          y1=crop_rect[1],
-                          width=crop_rect[2],
-                          height=crop_rect[3]) for x in vid_clips]
+        vid_clips = [
+            crop(
+                x,
+                x1=crop_rect[0],
+                y1=crop_rect[1],
+                width=crop_rect[2],
+                height=crop_rect[3],
+            )
+            for x in vid_clips
+        ]
 
     vid_clips = [v.margin(2) for v in vid_clips]
 
     min_duration = np.min([v.duration for v in vid_clips])
-    print('Set duration to min of all videos: %i' % min_duration)
+    print("Set duration to min of all videos: %i" % min_duration)
     vid_clips = [v.set_duration(min_duration) for v in vid_clips]
 
     def create_text_clip(text, dura):
         global src
-        return TextClip(text,
-                        font='Verdana',
-                        fontsize=max_h / 20,
-                        color='white') \
-            .set_duration(dura)
+        return TextClip(
+            text, font="Verdana", fontsize=max_h / 20, color="white"
+        ).set_duration(dura)
 
     if titles is None:
         titles = [os.path.splitext(os.path.basename(x))[0] for x in vid_files]
@@ -54,8 +62,8 @@ def generate_video_matrix(vid_files, titles=None, out_file=None, columns=None, f
     arr = []
     if columns is not None:
         for i in range(0, len(vid_clips), columns):
-            arr.append(vid_clips[i:i + columns])
-            arr.append(text_clips[i:i + columns])
+            arr.append(vid_clips[i : i + columns])
+            arr.append(text_clips[i : i + columns])
 
         remainder = len(vid_clips) % columns
         if remainder != 0:
@@ -73,7 +81,7 @@ def generate_video_matrix(vid_files, titles=None, out_file=None, columns=None, f
     final.write_videofile(out_file, fps=fps)
 
 
-def make_video(images, fps=30, out_file='output.mp4', format='bgr24'):
+def make_video(images, fps=30, out_file="output.mp4", format="bgr24"):
     ps = None
 
     for im in images:
@@ -81,27 +89,38 @@ def make_video(images, fps=30, out_file='output.mp4', format='bgr24'):
             w = im.shape[1]
             h = im.shape[0]
             command = [
-                'ffmpeg',
-                '-y',  # (optional) overwrite output file if it exists
-                '-f', 'rawvideo',
+                "ffmpeg",
+                "-y",  # (optional) overwrite output file if it exists
+                "-f",
+                "rawvideo",
                 # '-vcodec', 'rawvideo',
-                '-s', f'{w}x{h}',  # size of one frame
-                '-pix_fmt', format]
+                "-s",
+                f"{w}x{h}",  # size of one frame
+                "-pix_fmt",
+                format,
+            ]
 
             if fps and fps > 0:
-                command += ['-r', str(fps)]
+                command += ["-r", str(fps)]
 
             command += [
-                '-i', '-',  # The imput comes from a pipe
-                '-an',  # Tells FFMPEG not to expect any audio
-
+                "-i",
+                "-",  # The imput comes from a pipe
+                "-an",  # Tells FFMPEG not to expect any audio
                 # '-vcodec', 'rawvideo',
                 # '-vcodec', 'huffyuv',
                 # '-c:v', 'libx264', '-preset', 'slow', '-crf', '0',
-                '-c:v', 'h264_nvenc', '-profile', 'high444p', '-pixel_format', 'yuv444p', '-preset', 'default',
+                "-c:v",
+                "h264_nvenc",
+                "-profile",
+                "high444p",
+                "-pixel_format",
+                "yuv444p",
+                "-preset",
+                "default",
                 # '-hwaccel', 'cuvid',
                 # '-vf','scale=-2:720',
-                out_file
+                out_file,
             ]
 
             ps = subprocess.Popen(command, stdin=subprocess.PIPE)
@@ -112,69 +131,102 @@ def make_video(images, fps=30, out_file='output.mp4', format='bgr24'):
 
 
 def _get_media_duration(f):
-    dura = subprocess.check_output([
-        'ffprobe',
-        '-v', 'error',
-        '-show_entries', 'format=duration',
-        '-of', 'default=noprint_wrappers=1:nokey=1',
-        f
-    ], universal_newlines=True)
+    dura = subprocess.check_output(
+        [
+            "ffprobe",
+            "-v",
+            "error",
+            "-show_entries",
+            "format=duration",
+            "-of",
+            "default=noprint_wrappers=1:nokey=1",
+            f,
+        ],
+        universal_newlines=True,
+    )
     return float(dura)
 
 
-def ffmpeg(in_file, out_file='out.mp4', start_and_duration=None, reencode=False, nvenc=True, extra_args=None,
-           quality=100, no_output=False, crf=None, preset='slow', bitrate=None, max_size_mb=None, no_audio=False):
+def ffmpeg(
+    in_file,
+    out_file="out.mp4",
+    start_and_duration=None,
+    reencode=False,
+    nvenc=True,
+    extra_args=None,
+    quality=100,
+    no_output=False,
+    crf=None,
+    preset="slow",
+    bitrate=None,
+    max_size_mb=None,
+    no_audio=False,
+):
     if crf and crf < 19:
-        raise Exception(
-            'ERROR: 19 is visually identical to 0. Do not go lower.')
+        raise Exception("ERROR: 19 is visually identical to 0. Do not go lower.")
 
     if max_size_mb:
-        bitrate = '%.0fk' % (max_size_mb * 8192 / _get_media_duration(in_file))
+        bitrate = "%.0fk" % (max_size_mb * 8192 / _get_media_duration(in_file))
 
-    args = ['ffmpeg',
-            '-i', in_file]
+    args = ["ffmpeg"]
+
+    args += ["-i", in_file]
 
     if no_output:
-        args += ['-hide_banner', '-loglevel', 'panic']
+        args += ["-hide_banner", "-loglevel", "panic"]
 
     if start_and_duration:
-        args += ['-ss', str(start_and_duration[0]),
-                 '-strict', '-2',
-                 '-t', str(start_and_duration[1])]
+        args += [
+            "-ss",
+            str(start_and_duration[0]),
+            "-strict",
+            "-2",
+            "-t",
+            str(start_and_duration[1]),
+        ]
 
     if extra_args:
         args += extra_args
 
     if reencode:
         if nvenc:
-            args += ['-c:v', 'h264_nvenc']
+            args += ["-c:v", "h264_nvenc"]
             if not bitrate and crf:
-                args += ['-rc:v', 'vbr_hq', '-cq:v', '%d' % crf]
+                args += ["-rc:v", "vbr_hq", "-cq:v", "%d" % crf]
         else:
-            args += ['-c:v', 'libx264']
+            args += ["-c:v", "libx264"]
             if not bitrate and crf:
-                args += ['-crf', '%d' % crf]
+                args += ["-crf", "%d" % crf]
 
-        args += ['-preset', preset]
+        args += ["-preset", preset]
 
-        args += ['-pix_fmt', 'yuv420p']  # Wide used pixel format
+        args += ["-pix_fmt", "yuv420p"]  # Wide used pixel format
 
         if bitrate:
             args += [
-                '-b:v', bitrate,
-                '-maxrate', bitrate,
-                '-bufsize', bitrate,
+                "-b:v",
+                bitrate,
+                "-maxrate",
+                bitrate,
+                "-bufsize",
+                bitrate,
             ]
 
             if 1:
-                subprocess.call(args + [
-                    '-pass', '1',
-                    '-an',
-                    '-f', 'mp4',
-                    'nul' if sys.platform == 'win32' else '/dev/null', '-y'
-                ])  # first pass
+                subprocess.call(
+                    args
+                    + [
+                        "-pass",
+                        "1",
+                        "-an",
+                        "-f",
+                        "mp4",
+                        "nul" if sys.platform == "win32" else "/dev/null",
+                        "-y",
+                    ]
+                )  # first pass
 
-                args += ['-pass', '2']
+                args += ["-pass", "2"]
 
             # args += [
             #     '-maxrate', bitrate,
@@ -182,33 +234,37 @@ def ffmpeg(in_file, out_file='out.mp4', start_and_duration=None, reencode=False,
             # ]
 
         if no_audio:
-            args += ['-an']
+            args += ["-an"]
         else:
-            args += [
-                '-c:a', 'aac',
-                '-b:a', '128k'
-            ]  # Audio
+            args += ["-c:a", "aac", "-b:a", "128k"]  # Audio
 
-    args += [out_file, '-y']  # Override file
+    args += [out_file, "-y"]  # Override file
 
-    print('> ' + ' '.join(args))
+    print("> " + " ".join(args))
     subprocess.check_call(args)
 
 
-def extract_imgs(f, fps=1, out_folder='out'):
+def extract_imgs(f, fps=1, out_folder="out"):
     os.makedirs(out_folder, exist_ok=True)
 
-    subprocess.check_call([
-        'ffmpeg',
-        '-hwaccel', 'cuvid',
-        # '-ss', '50',
-        '-i', f,
-        '-an',
-        # '-vf', f'fps=1/60',
-        # '-vf', "select='not(mod(n,800))'",
-        '-vf', "select='eq(pict_type,PICT_TYPE_I)'",
-        # '-vframes:v', '1',
-        '-vsync', 'vfr',
-        '-qscale:v', '2',
-        os.path.join(out_folder, "%04d.jpg"),
-    ])
+    subprocess.check_call(
+        [
+            "ffmpeg",
+            "-hwaccel",
+            "cuvid",
+            # '-ss', '50',
+            "-i",
+            f,
+            "-an",
+            # '-vf', f'fps=1/60',
+            # '-vf', "select='not(mod(n,800))'",
+            "-vf",
+            "select='eq(pict_type,PICT_TYPE_I)'",
+            # '-vframes:v', '1',
+            "-vsync",
+            "vfr",
+            "-qscale:v",
+            "2",
+            os.path.join(out_folder, "%04d.jpg"),
+        ]
+    )
---
+++ scripts/r/video/image_seq_to_video.py
@@ -0,0 +1,11 @@
+from _video import *
+from _shutil import *
+
+
+fps = "{{_FPS}}" if "{{_FPS}}" else "60"
+
+cd(os.environ["CURRENT_FOLDER"])
+
+exec_bash(
+    f"cat *.jpg | ffmpeg -f image2pipe -r {fps} -vcodec mjpeg -i - -vcodec libx264 out.mp4 -y"
+)
