198264d1ef0379bc7769431149bfcd5b19f7249f
rossning92 <rossning92@github.com>
2020-04-29 22:08:18 -0700

    message

--- scripts/r/video/ccapture_to_mov.py
+++ scripts/r/video/ccapture_to_mov.py
@@ -2,90 +2,114 @@ from _shutil import *
 
 
 def _images_to_video(out_file, image_files, fps):
-    txt_body = '\n'.join(["file '%s'" % x for x in image_files])
-    txt_file = write_temp_file(txt_body, '.txt')
+    txt_body = "\n".join(["file '%s'" % x for x in image_files])
+    txt_file = write_temp_file(txt_body, ".txt")
 
-    if 1:
-        call2([
-            'ffmpeg',
-            '-r', str(fps),
-            '-f', 'concat', '-safe', '0',
-            '-i', txt_file,
-            '-vcodec', 'prores_ks',
-            '-pix_fmt', 'yuva444p10le',
-            '-alpha_bits', '16',
-            '-profile:v', '4444',
-            '-f', 'mov',
-            out_file, '-y'
-        ])
+    if out_file.endswith(".mov"):
+        call2(
+            [
+                "ffmpeg",
+                "-r",
+                str(fps),
+                "-f",
+                "concat",
+                "-safe",
+                "0",
+                "-i",
+                txt_file,
+                "-vcodec",
+                "prores_ks",
+                "-pix_fmt",
+                "yuva444p10le",
+                "-alpha_bits",
+                "16",
+                "-profile:v",
+                "4444",
+                "-f",
+                "mov",
+                out_file,
+                "-y",
+            ]
+        )
+    elif out_file.endswith(".mp4"):
+        call2(
+            [
+                "ffmpeg",
+                "-r",
+                str(fps),
+                "-f",
+                "concat",
+                "-safe",
+                "0",
+                "-i",
+                txt_file,
+                "-vcodec",
+                "libx264",
+                "-preset",
+                "fast",
+                "-crf",
+                "19",
+                out_file,
+                "-y",
+            ]
+        )
     else:
-        call2([
-            'ffmpeg',
-            '-r', str(fps),
-            '-f', 'concat', '-safe', '0',
-            '-i', txt_file,
-            '-vcodec', 'libx264',
-            '-preset', 'fast',
-            '-crf', '22',
-            '-f', 'mov',
-            out_file, '-y'
-        ])
+        raise Exception("invalid format: %s" % out_file)
 
 
 def convert_to_mov(tar_file, fps, out_file=None):
     out_files = []
-    name = os.path.splitext(tar_file)[0]
-    if out_file is None:
-        out_file = name
 
-    tmp_folder = tempfile.gettempdir() + os.path.sep + \
-        os.path.basename(name)
+    name, ext = os.path.splitext(tar_file)
+    if out_file is None:
+        out_file = name + '.mov'
+
+    tmp_folder = tempfile.gettempdir() + os.path.sep + os.path.basename(name)
 
     # Unzip
-    print2('Unzip to %s' % tmp_folder)
+    print2("Unzip to %s" % tmp_folder)
     shutil.unpack_archive(tar_file, tmp_folder)
-    print2(tmp_folder, color='red')
+    print2(tmp_folder, color="red")
 
     # Get all image files
-    image_files = sorted(glob.glob((os.path.join(tmp_folder, '*.png'))))
+    image_files = sorted(glob.glob((os.path.join(tmp_folder, "*.png"))))
 
     # Meta file
     meta = None
-    if os.path.exists(name + '.json'):
-        with open(name + '.json', 'r', encoding='utf-8') as f:
+    if os.path.exists(name + ".json"):
+        with open(name + ".json", "r", encoding="utf-8") as f:
             meta = json.load(f)
 
-    if meta and len(meta['cutPoints']) > 0:
+    if meta and len(meta["cutPoints"]) > 0:
         # convert to index
-        indices = [int(round(x * fps)) for x in meta['cutPoints']]
+        indices = [int(round(x * fps)) for x in meta["cutPoints"]]
         indices.insert(0, 0)  # prepend frame 0
         indices.append(len(image_files))  # append last frame index + 1
 
         n = 0
-        for i in range(len(indices)-1):
-            sub_image_files = image_files[indices[i]:indices[i+1]]
+        for i in range(len(indices) - 1):
+            sub_image_files = image_files[indices[i] : indices[i + 1]]
 
-            f = ('%s.mov' % out_file) if (n == 0) else ('%s.%d.mov' % (out_file, n))
+            f = (name + ext) if (n == 0) else ("%s.%d%s" % (name, n, ext))
             _images_to_video(f, sub_image_files, fps)
             out_files.append(f)
 
             n += 1
 
     else:
-        f = '%s.mov' % out_file
-        _images_to_video(f, image_files, fps)
-        out_files.append(f)
+        _images_to_video(out_file, image_files, fps)
+        out_files.append(out_file)
 
     remove(tmp_folder)
 
     return out_files
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     f = sys.argv[-1]
     name, ext = os.path.splitext(f)
-    if ext != '.tar':
-        print('Input file should be a .tar file')
+    if ext != ".tar":
+        print("Input file should be a .tar file")
         sys.exit(1)
 
-    convert_to_mov(f, fps=int('{{_FPS}}'))
+    convert_to_mov(f, fps=int("{{_FPS}}"))
--- scripts/r/web/animation/capture_animation.py
+++ scripts/r/web/animation/capture_animation.py
@@ -11,39 +11,48 @@ import time
 
 def capture_js_animation(url, out_file=None, output_video_file=True):
     if out_file is None:
-        out_file = 'animation_%s.mov' % get_time_str()
+        out_file = "animation_%s.mov" % get_time_str()
     prefix, ext = os.path.splitext(out_file)
 
     async def main():
-        browser = await launch(headless=False,
-                               args=['--disable-dev-shm-usage'],
-                               executablePath=r"C:\Program Files (x86)\Chromium\Application\chrome.exe")
+        browser = await launch(
+            headless=False,
+            args=["--disable-dev-shm-usage"],
+            executablePath=r"C:\Program Files (x86)\Chromium\Application\chrome.exe",
+        )
         page = await browser.newPage()
 
-        await page._client.send('Page.setDownloadBehavior', {
-            'behavior': 'allow',
-            'downloadPath': os.path.abspath(os.path.dirname(prefix))
-        })
+        await page._client.send(
+            "Page.setDownloadBehavior",
+            {
+                "behavior": "allow",
+                "downloadPath": os.path.abspath(os.path.dirname(prefix)),
+            },
+        )
 
         await page.goto(url)
 
         time.sleep(1.0)
 
-        print('Start capture.')
-        await page.evaluate('() => { window.startCapture({name: "%s"}); }' % os.path.basename(prefix))
+        print("Start capture.")
+        await page.evaluate(
+            '() => { window.startCapture({name: "%s"}); }' % os.path.basename(prefix)
+        )
 
-        await page.waitForXPath("//*[@id='captureStatus' and contains(., 'stopped')]", timeout=0)
+        await page.waitForXPath(
+            "//*[@id='captureStatus' and contains(., 'stopped')]", timeout=0
+        )
 
-        while not exists(prefix + '.tar'):
+        while not exists(prefix + ".tar"):
             time.sleep(0.5)
 
         await browser.close()
 
     asyncio.get_event_loop().run_until_complete(main())
 
-    tar_file = prefix + '.tar'
+    tar_file = prefix + ".tar"
     if output_video_file:
-        result = convert_to_mov(tar_file, fps=25, out_file=prefix)
+        result = convert_to_mov(tar_file, fps=25, out_file=prefix+ext)
         os.remove(tar_file)
     else:
         result = tar_file
@@ -52,6 +61,8 @@ def capture_js_animation(url, out_file=None, output_video_file=True):
 
 
 if __name__ == "__main__":
-    cd(expanduser('~/Downloads'))
-    capture_js_animation('http://localhost:8080/' + '{{HTML_FILE}}',
-                         output_video_file=bool('{{_OUTPUT_VID_FILE}}'))
+    cd(expanduser("~/Downloads"))
+
+    out_file = os.path.splitext("{{HTML_FILE}}")[0] + ".mp4"
+    print('output: %s' % out_file)
+    capture_js_animation("http://localhost:8080/" + "{{HTML_FILE}}", out_file=out_file)
