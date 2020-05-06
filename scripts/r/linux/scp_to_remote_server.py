ae0c022406bd856fb6ad44487076c475a5b1d415
rossning92 <rossning92@github.com>
2020-05-05 20:16:24 -0700

    commit with no message.

--- scripts/r/linux/scp_to_remote_server.py
+++ scripts/r/linux/scp_to_remote_server.py
@@ -1,15 +1,17 @@
 from _shutil import *
 
+if '{{_DST_DIR}}':
+    dst = '{{_DST_DIR}}'
+else:
+    dst = '/home/{{SSH_USER}}'
 
 call_echo(
     [
         "pscp",
         "-pw",
         "{{SSH_PWD}}",
-        "-P",
-        "1234",
         get_files()[0],
-        "{{SSH_USER}}@localhost:/home/{{SSH_USER}}",
+        "{{SSH_USER}}@{{SSH_HOST}}:" + dst,
     ]
 )
 
