02e2d99751dca2818866738600d505824794e201
rossning92 <rossning92@github.com>
2020-05-10 15:31:31 -0700

    commit with no message.

--- scripts/r/web/carbon_gen_code_image.py
+++ scripts/r/web/carbon_gen_code_image.py
@@ -3,66 +3,73 @@ from _shutil import *
 IMG_SIZE = (1920, 1080)
 BORDER = 2
 
-CONFIG = '''
-
-'''
-
 setup_nodejs(install=True)
 
-with open(expanduser('~/.carbon-now.json'), 'w') as f:
-    json.dump({
-        "latest-preset": {
-            "t": "3024-night",
-            "bg": "#ADB7C1",
-            "wt": "none",
-            "wc": True,
-            "fm": "Hack",
-            "fs": "18px",
-            "ln": False,
-            "ds": True,
-            "dsyoff": "3px",
-            "dsblur": "50px",
-            "wa": True,
-            "lh": "133%",
-            "pv": "100px",
-            "ph": "100px",
-            "si": False,
-            "wm": False,
-            "es": "4x",
-            "type": "png"
-        }
-    }, f)
 
-f = get_files(cd=True)[0]
-name = os.path.splitext(f)[0]
+def gen_image(code, out_file):
+    with open(expanduser("~/.carbon-now.json"), "w") as f:
+        json.dump(
+            {
+                "latest-preset": {
+                    "t": "3024-night",
+                    "bg": "#ADB7C1",
+                    "wt": "none",
+                    "wc": False,  # window controls
+                    "fm": "Hack",
+                    "fs": "14px",
+                    "ln": True,  # line no
+                    "ds": True,  # drop shadow
+                    "dsyoff": "3px",
+                    "dsblur": "50px",
+                    "wa": False,  # width adjustment
+                    "lh": "133%",
+                    "pv": "100px",
+                    "ph": "100px",
+                    "si": True,  # Squared image
+                    "wm": False,  # Watermark
+                    "es": "2x",
+                    "type": "png",
+                }
+            },
+            f,
+        )
 
-try:
-    call2(f'carbon-now {f} -t {name}')
-except:
-    print2('Please re-run...', color='red')
-    call2('npm i -g carbon-now-cli')
-    exit(0)
+    tmp_file = write_temp_file(code, ".txt")
 
-from PIL import Image, ImageDraw
+    try:
+        call2(f"carbon-now {tmp_file} -t {os.path.splitext(out_file)[0]}")
+    except:
+        print2("Please re-run...", color="red")
+        call2("yarn global add carbon-now-cli")
+        exit(0)
 
-im2 = Image.open(name + '.png')
-im2 = im2.crop((BORDER,
-                BORDER,
-                im2.width - 2 * BORDER,
-                im2.height - 2 * BORDER))
+    from PIL import Image, ImageDraw
 
-top_left = ((IMG_SIZE[0] - im2.width) // 2 + BORDER,
-            (IMG_SIZE[1] - im2.height) // 2 + BORDER)
-print(top_left)
+    im2 = Image.open(out_file)
+    im2 = im2.crop((BORDER, BORDER, im2.width - 2 * BORDER, im2.height - 2 * BORDER))
 
-im = Image.new('RGB', IMG_SIZE)
+    top_left = (
+        (IMG_SIZE[0] - im2.width) // 2 + BORDER,
+        (IMG_SIZE[1] - im2.height) // 2 + BORDER,
+    )
+    print(top_left)
 
-draw = ImageDraw.Draw(im)
-draw.rectangle((0, 0, IMG_SIZE[0], IMG_SIZE[1]), fill=0xC1B7AD)
-del draw
+    im = Image.new("RGB", IMG_SIZE)
 
-im.paste(im2, top_left)
+    draw = ImageDraw.Draw(im)
+    draw.rectangle((0, 0, IMG_SIZE[0], IMG_SIZE[1]), fill=0xC1B7AD)
+    del draw
 
-# write to stdout
-im.save(f'{name}.png')
-call2(f'start {name}.png')
+    im.paste(im2, top_left)
+
+    # write to stdout
+    im.save(out_file)
+    # call2(f"start {out_file}.png")
+
+
+if __name__ == "__main__":
+    file = get_files(cd=True)[0]
+    s = open(file, encoding="utf-8").read()
+
+    out_file = os.path.splitext(file)[0] + ".png"
+    gen_image(s, out_file)
