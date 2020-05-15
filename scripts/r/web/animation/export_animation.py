22382003e4f54c189cdff6d91ec5695d99a866a7
rossning92 <rossning92@github.com>
2020-05-14 22:13:30 -0700

    commit with no message.

--- scripts/r/web/animation/export_animation.py
+++ scripts/r/web/animation/export_animation.py
@@ -646,9 +646,9 @@ def screencap(f, speed=None, track=None, **kwargs):
     )
 
 
-def md(s, track="md", fadein=True, fadeout=True, pos="center", **kwargs):
+def md(s, track="md", fadein=True, fadeout=True, pos="center", name=None, **kwargs):
     mkdir("tmp/slides")
-    out_file = "tmp/slides/%s.png" % slugify(s)
+    out_file = "tmp/slides/%s.png" % slugify(name if name else s)
 
     if not os.path.exists(out_file):
         generate_slide(
