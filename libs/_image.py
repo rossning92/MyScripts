81adf97fda1474351b684059471ab806fba9feff
Ross Ning <rossning92@gmail.com>
2020-05-21 18:05:44 -0700

    commit with no message.

--- libs/_image.py
+++ libs/_image.py
@@ -3,6 +3,7 @@ import glob
 import os
 import math
 import re
+import numpy as np
 
 
 def crop_image_file(file_name, rect=None, rect_normalized=None):
@@ -13,6 +14,7 @@ def crop_image_file(file_name, rect=None, rect_normalized=None):
 
 def crop_image(im, rect=None, rect_normalized=None):
     import numpy as np
+
     if type(im) == np.ndarray:
         h, w = im.shape[0:2]
     else:
@@ -23,12 +25,12 @@ def crop_image(im, rect=None, rect_normalized=None):
             rect_normalized[0] * w,
             rect_normalized[1] * h,
             rect_normalized[2] * w,
-            rect_normalized[3] * h
+            rect_normalized[3] * h,
         ]
         rect = [int(x) for x in rect]
 
     if type(im) == np.ndarray:
-        im = im[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]]
+        im = im[rect[1] : rect[1] + rect[3], rect[0] : rect[0] + rect[2]]
     else:
         im = im.crop((rect[0], rect[1], rect[0] + rect[2], rect[1] + rect[3]))
 
@@ -39,20 +41,31 @@ def scale_image(im, sx, sy):
     return im.resize((int(sx * im.size[0]), int(sy * im.size[1])), Image.ANTIALIAS)
 
 
-def show_im(*imgs, format='rgb', out_image_name=None, origin='upper', text=None):
+def show_im(
+    *imgs,
+    format="rgb",
+    out_image_name=None,
+    origin="upper",
+    text=None,
+    norm=False,
+    split_channels=False
+):
     import matplotlib.pyplot as plt
 
-    plt.style.use('dark_background')
+    if split_channels:
+        imgs = [imgs[0][:, :, i] for i in range(imgs[0].shape[-1])]
+        print(imgs)
+
+    plt.style.use("dark_background")
 
     fig = plt.figure(figsize=(len(imgs) * 4, 1 * 4))
     for i, im in enumerate(imgs):
-        if len(im.shape) == 3 and im.shape[2] == 3 and format == 'bgr':
+        if len(im.shape) == 3 and im.shape[2] == 3 and format == "bgr":
             im = im[..., ::-1]
 
         # Visualize two channel image (e.g. optical flow, vector)
         if len(im.shape) == 3 and im.shape[2] == 2:
             import cv2
-            import numpy as np
 
             hsv = np.zeros([im.shape[0], im.shape[1], 3])
             hsv[..., 1] = 255
@@ -63,8 +76,13 @@ def show_im(*imgs, format='rgb', out_image_name=None, origin='upper', text=None)
             im = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
         ax = fig.add_subplot(1, len(imgs), i + 1)
+
         if text:
             ax.title.set_text(text[i])
+
+        # if norm:
+        #     im = np.mean(im.astype(float), axis=2)
+
         plt.imshow(im, origin=origin)
 
     plt.tight_layout()
@@ -77,17 +95,19 @@ def show_im(*imgs, format='rgb', out_image_name=None, origin='upper', text=None)
     plt.close(fig)
 
 
-def draw_text(im, text, box, text_outline=2, font_color='white', align='center', font_scale=1.0):
+def draw_text(
+    im, text, box, text_outline=2, font_color="white", align="center", font_scale=1.0
+):
     PADDING = 4
 
     draw = ImageDraw.Draw(im)
     font = ImageFont.truetype("arial.ttf", int(box[3] / 16 * font_scale))
     w, h = draw.multiline_textsize(text, font=font)
 
-    if align == 'top':
+    if align == "top":
         x = box[0] + (box[2] - w) / 2
         y = box[1] + PADDING
-    elif align == 'topLeft':
+    elif align == "topLeft":
         x = box[0] + PADDING
         y = box[1] + PADDING
     else:  # center
@@ -96,22 +116,36 @@ def draw_text(im, text, box, text_outline=2, font_color='white', align='center',
 
     t = text_outline
     for dx, dy in [[0, t], [t, 0], [-t, 0], [0, -t]]:
-        draw.multiline_text((x + dx, y + dy), text,
-                            font=font, fill="black", align='center')
-    draw.multiline_text((x, y), text, font=font,
-                        fill=font_color, align='center')
+        draw.multiline_text(
+            (x + dx, y + dy), text, font=font, fill="black", align="center"
+        )
+    draw.multiline_text((x, y), text, font=font, fill=font_color, align="center")
 
     del draw
 
 
-def combine_images(image_files=None, images=None, out_file=None, parse_file_name=None, cols=4, spacing=4, scale=1.0,
-                   text_outline=2,
-                   gif_duration=500, generate_atlas=True, generate_gif=True, draw_label=True, labels=None,
-                   label_align='top',
-                   title=None, title_align='top', title_color='white',
-                   font_color='white',
-                   col_major_order=False,
-                   font_scale=1.0):
+def combine_images(
+    image_files=None,
+    images=None,
+    out_file=None,
+    parse_file_name=None,
+    cols=4,
+    spacing=4,
+    scale=1.0,
+    text_outline=2,
+    gif_duration=500,
+    generate_atlas=True,
+    generate_gif=True,
+    draw_label=True,
+    labels=None,
+    label_align="top",
+    title=None,
+    title_align="top",
+    title_color="white",
+    font_color="white",
+    col_major_order=False,
+    font_scale=1.0,
+):
     file_list = None
     if image_files:
         if type(image_files) == list:
@@ -121,20 +155,23 @@ def combine_images(image_files=None, images=None, out_file=None, parse_file_name
             file_list = [x for x in file_list if os.path.isfile(x)]
 
         if len(file_list) == 0:
-            raise Exception('No image files has been found: %s' % image_files)
+            raise Exception("No image files has been found: %s" % image_files)
 
         imgs = [Image.open(f) for f in file_list]
         if scale != 1.0:
-            imgs = [im.resize(
-                (int(im.width * scale), int(im.height * scale)), Image.NEAREST) for im in imgs]
+            imgs = [
+                im.resize(
+                    (int(im.width * scale), int(im.height * scale)), Image.NEAREST
+                )
+                for im in imgs
+            ]
 
     elif images:
         # Convert to PIL image
-        imgs = [Image.fromarray(x, 'RGB') for x in images]
+        imgs = [Image.fromarray(x, "RGB") for x in images]
 
     else:
-        raise Exception(
-            "`image_files` and `images` cannot be None at the same time.")
+        raise Exception("`image_files` and `images` cannot be None at the same time.")
 
     if not cols:
         cols = math.ceil(math.sqrt(len(imgs)))
@@ -155,13 +192,20 @@ def combine_images(image_files=None, images=None, out_file=None, parse_file_name
                 if parse_file_name is not None:
                     text = parse_file_name(text)
                 else:
-                    text = text.replace('_', ' ')
+                    text = text.replace("_", " ")
             else:
                 draw_label = False
 
             if draw_label:
-                draw_text(im, text, (0, 0, imgs[0].width, imgs[0].height), text_outline, font_color,
-                          align=label_align, font_scale=font_scale)
+                draw_text(
+                    im,
+                    text,
+                    (0, 0, imgs[0].width, imgs[0].height),
+                    text_outline,
+                    font_color,
+                    align=label_align,
+                    font_scale=font_scale,
+                )
 
     if generate_atlas:
         num_imgs = len(imgs)
@@ -171,9 +215,13 @@ def combine_images(image_files=None, images=None, out_file=None, parse_file_name
             rows = cols
             cols = t
 
-        im_combined = Image.new('RGB',
-                                (imgs[0].width * cols + spacing * (cols - 1),
-                                 imgs[0].height * rows + spacing * (rows - 1)))
+        im_combined = Image.new(
+            "RGB",
+            (
+                imgs[0].width * cols + spacing * (cols - 1),
+                imgs[0].height * rows + spacing * (rows - 1),
+            ),
+        )
 
         for c in range(len(imgs)):
             if not col_major_order:
@@ -189,13 +237,15 @@ def combine_images(image_files=None, images=None, out_file=None, parse_file_name
             c += 1
 
         if title is not None:
-            draw_text(im_combined,
-                      title,
-                      (0, 0, im_combined.width, im_combined.height),
-                      text_outline,
-                      title_color,
-                      align=title_align,
-                      font_scale=font_scale)
+            draw_text(
+                im_combined,
+                title,
+                (0, 0, im_combined.width, im_combined.height),
+                text_outline,
+                title_color,
+                align=title_align,
+                font_scale=font_scale,
+            )
 
     if out_file:
         out_file = os.path.splitext(out_file)[0]  # Remove file extension
@@ -204,29 +254,33 @@ def combine_images(image_files=None, images=None, out_file=None, parse_file_name
             os.makedirs(os.path.dirname(out_file), exist_ok=True)
 
         if generate_atlas:
-            im_combined.save(out_file + '.png')
+            im_combined.save(out_file + ".png")
 
         if generate_gif:
-            imgs[0].save(out_file + '.gif',
-                         save_all=True,
-                         append_images=imgs[1:],
-                         duration=gif_duration,
-                         quality=100,
-                         loop=0)  # Repeat forever
+            imgs[0].save(
+                out_file + ".gif",
+                save_all=True,
+                append_images=imgs[1:],
+                duration=gif_duration,
+                quality=100,
+                loop=0,
+            )  # Repeat forever
 
     import numpy
+
     return numpy.array(im_combined)
 
 
 def parse_file_name(s):
-    gain_x = re.findall('GainX\((.*?)\)', s)[0].replace('.00', '')
-    gain_y = re.findall('GainY\((.*?)\)', s)[0].replace('.00', '')
-    s = 'GainX: %s\nGainY: %s' % (gain_x, gain_y)
+    gain_x = re.findall("GainX\((.*?)\)", s)[0].replace(".00", "")
+    gain_y = re.findall("GainY\((.*?)\)", s)[0].replace(".00", "")
+    s = "GainX: %s\nGainY: %s" % (gain_x, gain_y)
     return s
 
 
 def select_region(image_file):
     import cv2
+
     im = cv2.imread(image_file)
 
     # Select ROI
--- scripts/r/view_raw_image.py
+++ scripts/r/view_raw_image.py
@@ -6,10 +6,13 @@ from _image import *
 f = get_files()[0]
 im = np.fromfile(f, dtype=np.uint8)
 
-shape = [{{_H}}, {{_W}}]
-if '{{_CHANNEL}}':
+width, height = [int(x) for x in "{{_SIZE}}".split()]
+
+shape = [height, width]
+if "{{_CHANNEL}}":
     shape.append({{_CHANNEL}})
+
 im = im.reshape(shape)
 print(im)
 
-show_im(im)
+show_im(im, split_channels=bool("{{_SPLIT_CHANNELS}}"))
