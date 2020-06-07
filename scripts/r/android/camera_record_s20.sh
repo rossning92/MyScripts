918ca33f053bcfe8363d8e00c3baa0bc0e89cccf
rossning92 <rossning92@github.com>
2020-06-06 18:01:30 -0700

    commit with no message.

---
+++ scripts/r/android/camera_record_s20.sh
@@ -0,0 +1,38 @@
+run_script /r/android/oneplus/unlock.sh
+
+# Set display timeout to 1 minute.
+adb shell settings put system screen_off_timeout 60000
+
+# Kill the camera app.
+adb shell am force-stop com.sec.android.app.camera
+
+# When phone is in portrait mode, default to back camera.
+adb shell am start -a android.media.action.VIDEO_CAPTURE
+echo 'Wait for 2s.'
+sleep 2
+
+# Press volume key to start recording.
+adb shell input keyevent KEYCODE_VOLUME_DOWN
+echo -e '\033[0;36mRecording started.\033[0m'
+
+echo 'Manual focus at center.'
+adb shell input tap 540 1296
+
+read -p 'Press <enter> to stop...'
+adb shell input keyevent KEYCODE_VOLUME_DOWN
+sleep 1
+
+echo 'Confirm saving the file.'
+adb shell input keyevent 66
+
+echo 'Wait for 2s.'
+sleep 2
+
+# Dump the file.
+file=$(adb shell 'ls /sdcard/DCIM/Camera' | tail -n 1)
+cd ~/Desktop
+adb pull /sdcard/DCIM/Camera/$file
+adb shell rm /sdcard/DCIM/Camera/$file
+
+# Preview the file.
+mpv $file
--- scripts/r/android/oneplus/unlock.sh
+++ scripts/r/android/oneplus/unlock.sh
@@ -5,7 +5,7 @@ if [[ $out == *"Dozing"* ]]; then
     sleep 1
     
 	# Swipe up
-	adb shell input touchscreen swipe 930 880 930 380
+	adb shell input touchscreen swipe 540 1000 540 400
 	sleep 1
 	
     adb shell input text 123475
