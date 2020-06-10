de92aa54f1b98a5d7a995bf6686e3b075142e1d7
rossning92 <rossning92@github.com>
2020-06-09 22:20:25 -0700

    commit with no message.

--- scripts/r/image/batch_conversion.py
+++ scripts/r/image/batch_conversion.py
@@ -2,26 +2,47 @@ from _shutil import *
 from _image import *
 import numpy as np
 
-cd(os.environ['CURRENT_FOLDER'])
+
+def crop_border(pil_image):
+    np_array = np.array(pil_image)
+    blank_px = [255, 255, 255, 0]
+    mask = np_array != blank_px
+    coords = np.argwhere(mask)
+    x0, y0, z0 = coords.min(axis=0)
+    x1, y1, z1 = coords.max(axis=0) + 1
+    cropped_box = np_array[x0:x1, y0:y1, z0:z1]
+    pil_image = Image.fromarray(cropped_box, "RGBA")
+    return pil_image
 
 
-mkdir('out')
+def resize(im, w=None, h=None):
+    if (w is None) and (h is not None):
+        w = round(h * im.width / im.height)
+    elif (w is not None) and (h is None):
+        h = round(w * im.height / im.width)
+    elif (w is None) and (h is None):
+        raise Exception("w and h cannot be both None")
 
-rect_arg = [float(x) for x in '{{_CROP_RECT}}'.split()]
-scale = [float(x) for x in '{{_SCALE}}'.split()]
-img_size = [int(x) for x in '{{_OUT_IMG_SIZE}}'.split()]
-keep_ratio = bool('{{_KEEP_RATIO}}')
+    return im.resize((w, h), resample=Image.BILINEAR)
 
 
-files = os.listdir('.')
-files = [x for x in files if x.endswith(
-    '.jpg') or x.endswith('.png') or x.endswith('.bmp')]
+rect_arg = [float(x) for x in "{{_CROP_RECT}}".split()]
+scale = [float(x) for x in "{{_SCALE}}".split()]
+img_size = [int(x) for x in "{{_OUT_IMG_SIZE}}".split()]
+keep_ratio = bool("{{_KEEP_RATIO}}")
+
+
+files = get_files(cd=True)
+files = [
+    x for x in files if x.endswith(".jpg") or x.endswith(".png") or x.endswith(".bmp")
+]
 
 crop = False
+mkdir("out")
 for f in files:
     im = Image.open(f)
 
-    if '{{_SELECT_CROP_RECT}}' and not crop:
+    if "{{_SELECT_CROP_RECT}}" and not crop:
         box = select_region(f)
         rect = box
         rect_normalized = None
@@ -55,10 +76,22 @@ for f in files:
 
         im = im.resize((img_size[0], img_size[1]), Image.ANTIALIAS)
 
+    if "{{_CROP_BORDER}}":
+        im = crop_border(im)
+
+    w = None
+    h = None
+    if "{{_RESIZE_W}}":
+        w = int("{{_RESIZE_W}}")
+    if "{{_RESIZE_H}}":
+        h = int("{{_RESIZE_H}}")
+    if w is not None or h is not None:
+        im = resize(im, w=w, h=h)
+
     name, ext = os.path.splitext(f)
-    if '{{_OUT_FORMAT}}':
-        ext = '.' + '{{_OUT_FORMAT}}'
+    if "{{_OUT_FORMAT}}":
+        ext = "." + "{{_OUT_FORMAT}}"
     out_file = name + ext
 
-    print('Output: out/' + out_file)
-    im.save('out/' + out_file, quality=90)
+    print("Output: out/" + out_file)
+    im.save("out/" + out_file, quality=90)
