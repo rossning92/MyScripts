77d29e3191db4fab0c9887a0f4f3e019d0b50b2a
rossning92 <rossning92@github.com>
2020-06-14 02:27:45 -0700

    commit with no message.

--- scripts/r/font_tool.py
+++ scripts/r/font_tool.py
@@ -3,10 +3,12 @@ from _shutil import *
 
 # Simplify / subset font by specify characters to include.
 cd(gettempdir())
-f = download(
-    'https://raw.githubusercontent.com/DavidSheh/CommonChineseCharacter/master/3500%E5%B8%B8%E7%94%A8%E5%AD%97.txt')
-print(f)
-text = open(f, encoding='utf-8').read()
-subprocess.check_call(['pyftsubset',
-                       r"C:\Users\Ross\Downloads\SourceHanSansCN-Medium.ttf",
-                       f'--text={text}'])
+# f = download(
+#     'https://raw.githubusercontent.com/DavidSheh/CommonChineseCharacter/master/3500%E5%B8%B8%E7%94%A8%E5%AD%97.txt')
+# print(f)
+
+f = os.path.join(os.path.realpath(os.path.dirname(__file__)), "common_cn_chars.txt")
+text = open(f, encoding="utf-8").read()
+subprocess.check_call(
+    ["pyftsubset", r"C:\Tmp\SourceHanSansCN-Medium.ttf", f"--text={text}"]
+)
