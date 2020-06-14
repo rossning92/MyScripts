305228310666d52bce8e420f699850bc94788068
rossning92 <rossning92@github.com>
2020-06-13 20:29:44 -0700

    commit with no message.

--- scripts/r/linux/ssh.sh
+++ scripts/r/linux/ssh.sh
@@ -2,4 +2,8 @@ if ! [ -x "$(command -v expect)" ]; then
     sudo apt-get install expect
 fi
 
-expect -c 'spawn ssh {{_USER}}@{{_HOST}}; expect "password:"; send "{{_PWD}}\r"; interact;'
+if [[ -z "{{_PWD}}" ]]; then
+    ssh {{_USER}}@{{_HOST}}
+else
+    expect -c 'spawn ssh {{_USER}}@{{_HOST}}; expect "password:"; send "{{_PWD}}\r"; interact;'
+fi
