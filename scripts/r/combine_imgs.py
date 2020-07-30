785dfa7f51b2c7c36e0e177811d2c2536416666a
Ross Ning <rossning92@gmail.com>
2020-07-30 14:46:28 -0700

    commit with no message.

--- scripts/r/combine_imgs.py
+++ scripts/r/combine_imgs.py
@@ -1,26 +1,27 @@
 from _image import *
 from _shutil import *
 
-os.chdir(env['CURRENT_FOLDER'])
 
-cols = int('{{_COLS}}') if '{{_COLS}}' else None
-col_major_order = True if '{{_COL_MAJOR_ORDER}}' else False
-draw_label = True if '{{_DRAW_LABEL}}' else False
-label_align = '{{_LABEL_ALIGN}}' if '{{_LABEL_ALIGN}}' else 'top'
-gif_gen = True if '{{_GIF_GENERATION}}' else False
-gif_duration = int('{{_GIF_DURA}}') if '{{_GIF_DURA}}' else 500
-font_scale = float('{{_FONT_SCALE}}') if '{{_FONT_SCALE}}' else 1.0
-font_color = '{{_FONT_COLOR}}' if '{{_FONT_COLOR}}' else 'white'
+cols = int("{{_COLS}}") if "{{_COLS}}" else None
+col_major_order = True if "{{_COL_MAJOR_ORDER}}" else False
+draw_label = True if "{{_DRAW_LABEL}}" else False
+label_align = "{{_LABEL_ALIGN}}" if "{{_LABEL_ALIGN}}" else "top"
+gif_gen = True if "{{_GIF_GENERATION}}" else False
+gif_duration = int("{{_GIF_DURA}}") if "{{_GIF_DURA}}" else 500
+font_scale = float("{{_FONT_SCALE}}") if "{{_FONT_SCALE}}" else 1.0
+font_color = "{{_FONT_COLOR}}" if "{{_FONT_COLOR}}" else "white"
 
-combine_images(image_files='*.*',
-               out_file='out/out.png',
-               cols=cols,
-               col_major_order=col_major_order,
-               draw_label=draw_label,
-               label_align=label_align,
-               generate_gif=gif_gen,
-               gif_duration=gif_duration,
-               font_scale=font_scale,
-               font_color=font_color)
+combine_images(
+    image_files=get_files(cd=True),
+    out_file="out/out.png",
+    cols=cols,
+    col_major_order=col_major_order,
+    draw_label=draw_label,
+    label_align=label_align,
+    generate_gif=gif_gen,
+    gif_duration=gif_duration,
+    font_scale=font_scale,
+    font_color=font_color,
+)
 
-call2('start out/out.png')
+call2("start out/out.png")
