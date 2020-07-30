f2a8d054ddb07fc4dfbebb4519ad8dff8122027e
rossning92 <rossning92@github.com>
2020-07-29 23:02:39 -0700

    commit with no message.

---
+++ scripts/r/dev/vcpkg.py
@@ -0,0 +1,18 @@
+from _shutil import *
+
+cd("~")
+if not exists("vcpkg"):
+    call_echo("git clone https://github.com/microsoft/vcpkg")
+    call_echo(r".\vcpkg\bootstrap-vcpkg.bat")
+    # cd("vcpkg")
+
+while True:
+    print("s - search\n" "q - quit\n")
+    ch = getch()
+    if ch == "s":
+        kw = input("search for libaries:")
+        call_echo(r".\vcpkg\vcpkg search %s" % kw)
+
+    elif ch == "q":
+        sys.exit(0)
+
