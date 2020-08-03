0751a300f133f3d2c060f44a825b8380553738af
Ross Ning <rossning92@gmail.com>
2020-08-03 16:51:17 -0700

    commit with no message.

--- scripts/r/UE4/android_console_command.py
+++ scripts/r/UE4/android_console_command.py
@@ -3,22 +3,22 @@ from _term import *
 from _ue4 import *
 
 commands = {
-    'r': 'vr.HeadTracking.Reset',
-    'm': 'ke * MoveCameraEvent',
-    '[': 'vr.PixelDensity 0.1',
-    ']': 'vr.PixelDensity 1.0',
-    'f': 'stat fps',
-    'u': 'stat unit',
+    "r": "vr.HeadTracking.Reset",
+    "m": "ke * MoveCameraEvent",
+    "[": "vr.PixelDensity 0.1",
+    "]": "vr.PixelDensity 1.0",
+    "f": "stat fps", # stat RHI
+    "u": "stat unit",
 }
 
 # Engine\Build\Android\Java\src\com\epicgames\ue4\ConsoleCmdReceiver.java
 while True:
     for k, v in commands.items():
-        print('%s - %s' % (k, v))
+        print("%s - %s" % (k, v))
 
     s = getch()
-    if s == '\r':
-        print2('Console command: ', end='')
+    if s == "\r":
+        print2("Console command: ", end="")
         cmd = input()
     elif s in commands:
         cmd = commands[s]
