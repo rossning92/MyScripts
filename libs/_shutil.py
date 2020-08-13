fb2bda06d2c9e230f2c5999d63e66481e34ea5b3
Ross Ning <rossning92@gmail.com>
2020-08-13 13:23:23 -0700

    commit with no message.

--- libs/_shutil.py
+++ libs/_shutil.py
@@ -215,8 +215,14 @@ def call2(args, check=True, shell=True, **kwargs):
 
 
 def call_echo(args, shell=True, check=True, **kwargs):
+    import shlex
+    
     print("> ", end="")
-    print2(str(args), color="cyan")
+    if type(args) == list:
+        s = " ".join([shlex.quote(x) for x in args])
+    else:
+        s = args
+    print2(s, color="cyan")
     subprocess.run(args, shell=shell, check=check, **kwargs)
 
 
