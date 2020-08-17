4666d2093f727ceb4361a48f8787479a24d49dcc
Ross Ning <rossning92@gmail.com>
2020-08-17 04:02:10 -0700

    commit with no message.

---
+++ scripts/r/image/magick_add_label.sh
@@ -0,0 +1,6 @@
+magick \
+    '~/Desktop/123.gif' \
+    -fill white -undercolor '#00000080' \
+    -gravity South \
+    -annotate +0+5 'your text here!!\nSecond line!!!\nThird line!!' \
+    '~/Desktop/1234.gif'
