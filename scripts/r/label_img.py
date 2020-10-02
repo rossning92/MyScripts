2870123f61cb67258d56283c9f94b80f59eb88e5
Ross Ning <rossning92@gmail.com>
2020-10-02 10:54:27 -0700

    commit with no message.

--- scripts/r/label_img.py
+++ scripts/r/label_img.py
@@ -5,15 +5,15 @@ from PIL import Image
 
 f = get_files(cd=True)[0]
 fn, ext = os.path.splitext(f)
-text_file = fn + '.txt'
+text_file = fn + ".txt"
 
-call(['notepad.exe', text_file])
-with open(text_file, 'r') as fp:
+call(["notepad.exe", text_file])
+with open(text_file, "r") as fp:
     text = fp.read()
 
 im = Image.open(f)
-font_scale = float('{{_FONT_SCALE}}') if '{{_FONT_SCALE}}' else 1.0
-font_color = '{{_FONT_COLOR}}' if '{{_FONT_COLOR}}' else 'white'
-draw_text(im, text, (0, 0, im.width, im.height), align='top', font_scale=font_scale, font_color=font_color)
+font_scale = float("{{_FONT_SCALE}}") if "{{_FONT_SCALE}}" else 1.0
+font_color = "{{_FONT_COLOR}}" if "{{_FONT_COLOR}}" else "white"
+draw_text(im, text, align="top", font_scale=font_scale, font_color=font_color)
 
-im.save(fn + '_labelled' + ext)
+im.save(fn + "_labelled" + ext)
