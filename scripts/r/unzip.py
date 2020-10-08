8d26c739e58837841a98574e18b739f1777f342a
Ross Ning <rossning92@gmail.com>
2020-10-08 12:40:47 -0700

    commit with no message.

--- scripts/r/unzip.py
+++ scripts/r/unzip.py
@@ -3,7 +3,7 @@ from _shutil import *
 
 file = get_files(cd=True)[0]
 
-if file.endswith(".tar.gz"):
+if file.endswith(".tar.gz") or file.endswith(".tgz"):
     out_dir = file.rstrip(".tar.gz")
     mkdir(out_dir)
     call2('tar xzvf "%s" -C "%s"' % (file, out_dir))
