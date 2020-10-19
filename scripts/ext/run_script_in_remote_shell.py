f0714ea399f8bcd4bc4503c47d60d92fbb51f30d
Ross Ning <rossning92@gmail.com>
2020-10-19 02:33:48 -0700

    commit with no message.

--- scripts/ext/run_script_in_remote_shell.py
+++ scripts/ext/run_script_in_remote_shell.py
@@ -1,54 +1,41 @@
 from _script import *
 
-if __name__ == '__main__':
-    script_path = os.environ['_SCRIPT_PATH']
+if __name__ == "__main__":
+    script_path = os.environ["_SCRIPT_PATH"]
 
     script = ScriptItem(script_path)
     update_script_acesss_time(script)
-    
-    s = script.render() + '\n'
-    tmp_script_file = write_temp_file(s, '.sh')
 
-    if script.ext != '.sh':
-        print('Script type is not supported: %s' % script.ext)
+    s = script.render() + "\n"
+    tmp_script_file = write_temp_file(s, ".sh")
+
+    if script.ext != ".sh":
+        print("Script type is not supported: %s" % script.ext)
         exit(0)
 
-    s = (
-        "cat > /tmp/script.sh <<'__EOF__'\n"
-        + s + '\n' +
-        '__EOF__\n'
-        'clear\n'
-        'bash /tmp/script.sh\n'
+    # Ctrl-C
+    call_echo(["wsl", "screen", "-S", "ssh_session", "-X", "stuff", "^C"], shell=False)
+
+    # Write shell commands to paste buffer
+    lines = [
+        "cat > /tmp/script.sh <<'__EOF__'",
+        *s.splitlines(),
+        "__EOF__",
+        "clear",
+        "bash /tmp/script.sh",
+    ]
+    tmp_file = write_temp_file("\n".join(lines) + "\n", "pastebuf.txt")
+    call_echo(
+        [
+            "wsl",
+            "screen",
+            "-S",
+            "ssh_session",
+            "-X",
+            "readbuf",
+            convert_to_unix_path(tmp_file, wsl=True),
+        ],
+        shell=False,
     )
-    set_clip(s)
-
-    # exec_ahk('''
-    #     WinActivate ahk_exe putty.exe
-    #     WinWaitActive ahk_exe putty.exe
-    #     if ErrorLevel
-    #         return
-
-    #     Send +{Ins}
-    # ''')
-
-    exec_ahk('''
-        WinActivate r/linux/et
-        WinWaitActive r/linux/et,, 2
-        if ErrorLevel
-            return
-
-        Send ^c
-        Sleep 500
-
-        Send ^v
-
-        ; Loop 2 {
-        ;    WinWaitActive, ahk_exe ConEmu64.exe, Continue?, 2
-        ;    if ErrorLevel
-        ;        return
-        ;    Send {Enter}
-        ; }
-
-        ; Execute the last line
-        ; Send {Enter}
-    ''')
+    exec_ahk("WinActivate r/linux/ssh")
+    call_echo(["wsl", "screen", "-S", "ssh_session", "-X", "paste", "."], shell=False)
