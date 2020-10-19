14efbb981ed0bfd46bc9517a1e5b850df114312d
Ross Ning <rossning92@gmail.com>
2020-10-19 00:12:59 -0700

    commit with no message.

--- scripts/r/linux/ssh.sh
+++ scripts/r/linux/ssh.sh
@@ -5,8 +5,16 @@ fi
 
 printf "Host *\nControlMaster auto\nControlPath ~/.ssh/master-%%r@%%h:%%p.socket\n" >~/.ssh/config
 
+# sudo /etc/init.d/screen-cleanup start
+screen -r ssh_session -X quit
+screen -dmS ssh_session bash
+
 if [[ -z "{{_PWD}}" ]]; then
-    ssh {{_USER}}@{{_HOST}}
+    screen -r ssh_session -X stuff "ssh {{_USER}}@{{_HOST}}
+"
 else
-    expect -c 'set timeout -1; spawn ssh -o "StrictHostKeyChecking no" -R 5037:localhost:5037 {{_USER}}@{{_HOST}}; expect "password:"; send "{{_PWD}}\r"; interact;'
+    screen -r ssh_session -X stuff "expect -c 'set timeout -1; spawn ssh -o \"StrictHostKeyChecking no\" -R 5037:localhost:5037 {{_USER}}@{{_HOST}}; expect \"password:\"; send \"{{_PWD}}\r\"; interact;'
+"
 fi
+
+screen -r ssh_session
