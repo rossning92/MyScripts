431eeb55fbbbadcd170ae4b9986c5bf78d083785
rossning92 <rossning92@github.com>
2020-11-05 23:56:53 -0800

    commit with no message.

--- scripts/r/video/mpv/_mpv/scripts/my-video-tool.js
+++ scripts/r/video/mpv/_mpv/scripts/my-video-tool.js
@@ -1,8 +1,9 @@
-var inTime = 0.0;
-var outTime = 0.0;
 var historyFiles = [];
 var baseName = null;
 var currentFile = null;
+
+var inTime = 0;
+var outTime = 0;
 
 function setClip(text) {
   mp.utils.subprocess_detached({
@@ -14,38 +15,23 @@ function getBaseName(file) {
   return file.replace(/\.[^/.]+$/, "");
 }
 
-function set_in_time() {
-  inTime = mp.get_property_native("playback-time");
-  if (inTime == null || inTime == "none") {
-    inTime = 0;
+function setCutTime(setInTime) {
+  cur = mp.get_property_native("playback-time");
+  if (!outTime) {
+    outTime = mp.get_property_native("duration");
+  }
+
+  if (setInTime) {
+    inTime = cur;
+  } else {
+    outTime = cur;
   }
 
   if (inTime > outTime) {
     outTime = inTime;
   }
 
-  showCutInfo();
-}
-
-function set_out_time() {
-  outTime = mp.get_property_native("playback-time");
-  if (outTime == null || outTime == "none") {
-    outTime = 0;
-  }
-
-  if (outTime < inTime) {
-    inTime = outTime;
-  }
-
-  showCutInfo();
-}
-
-function showCutInfo() {
-  var message = "";
-  message += "begin=" + inTime.toFixed(3) + "s\n";
-  message += "end=" + outTime.toFixed(3) + "s\n";
-  message += "duration=" + (outTime - inTime).toFixed(3) + "s\n";
-  mp.osd_message(message);
+  mp.osd_message("[" + inTime.toFixed(3) + ", " + outTime.toFixed(3) + "]");
 }
 
 function getTimeStr() {
@@ -82,6 +68,17 @@ function exportVideo(params) {
 
   // Input file
   args = args.concat(["-i", currentFile]);
+
+  if (params.start != null) {
+    args = args.concat([
+      "-ss",
+      params.start.toString(),
+      "-strict",
+      "-2",
+      "-t",
+      params.duration.toString(),
+    ]);
+  }
 
   // Filters
   if (params.vf) {
@@ -125,84 +122,19 @@ function exportVideo(params) {
   var outFile = baseName + "-" + getTimestamp() + ".mp4";
   args.push(outFile);
 
-  mp.command_native({ name: "subprocess", args: args });
-  historyFiles.push(currentFile);
-  currentFile = outFile;
-
-  // HACK: delay play exported file
-  setTimeout(function () {
-    mp.commandv("loadfile", outFile);
-  }, 1);
-}
-
-function cut_video() {
-  var filePath = mp.get_property_native("path");
-  var fileName = mp.get_property_native("filename");
-  var fileNameNoExt = mp.get_property_native("filename/no-ext");
-  var extLen = fileName.length - fileNameNoExt.length;
-  var filePathNoExt = filePath.substring(0, filePath.length - extLen);
-
-  if (inTime == outTime) {
-    mp.osd_message("error: inTime == outTime");
-    return;
-  }
-
-  var outFile =
-    filePathNoExt +
-    "-cut-" +
-    inTime.toFixed(0) +
-    "-" +
-    outTime.toFixed(0) +
-    ".mp4";
-
-  var p = {};
-  p["cancellable"] = false;
-  p["args"] = [
-    "ffmpeg",
-    "-y",
-    "-nostdin",
-    "-i",
-    filePath,
-    "-ss",
-    inTime.toFixed(3),
-    "-strict",
-    "-2",
-    "-t",
-    (outTime - inTime).toFixed(3),
-    "-codec:v",
-    "libx264",
-    "-crf",
-    "19",
-    "-c:a",
-    "aac",
-    "-b:a",
-    "128k",
-    outFile,
-  ];
-
-  p["args"] = [
-    "ffmpeg",
-    "-ss",
-    inTime.toFixed(3),
-    "-i",
-    filePath,
-    "-t",
-    (outTime - inTime).toFixed(3),
-    "-c",
-    "copy",
-    outFile,
-  ];
-
-  var res = mp.utils.subprocess(p);
-  if (res["status"] != 0) {
-    var message = "Failed: status: " + res["status"];
-    if (res["error"] != null) {
-      message = message + ", error message: " + res["error"];
-    }
-    message = message + "\nstdout = " + res["stdout"];
-    mp.osd_message(message);
+  if (params.background) {
+    mp.utils.subprocess_detached({
+      args: args,
+    });
   } else {
-    mp.osd_message("Done.");
+    mp.command_native({ name: "subprocess", args: args });
+    historyFiles.push(currentFile);
+    currentFile = outFile;
+
+    // HACK: delay play exported file
+    setTimeout(function () {
+      mp.commandv("loadfile", outFile);
+    }, 1);
   }
 }
 
@@ -268,6 +200,17 @@ mp.add_forced_key_binding("ctrl+z", "undo", function () {
   }
 });
 
-// mp.add_forced_key_binding("i", "set_in_time", set_in_time);
-// mp.add_forced_key_binding("o", "set_out_time", set_out_time);
-// mp.add_forced_key_binding("x", "cut_video", cut_video);
+mp.add_forced_key_binding("[", "set_in_time", function () {
+  setCutTime(true);
+});
+mp.add_forced_key_binding("]", "set_out_time", function () {
+  setCutTime(false);
+});
+mp.add_forced_key_binding("x", "cut_video", function () {
+  mp.osd_message("cut video...");
+  exportVideo({ start: inTime, duration: outTime - inTime });
+});
+mp.add_forced_key_binding("X", "cut_video", function () {
+  mp.osd_message("cut video (background)...");
+  exportVideo({ start: inTime, duration: outTime - inTime, background: true });
+});
