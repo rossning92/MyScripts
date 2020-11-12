22ad952bf023b6512ff8aab54e0bac589f7cb2e5
Ross Ning <rossning92@gmail.com>
2020-11-11 19:27:41 -0800

    commit with no message.

--- scripts/r/android/restart_app.py
+++ scripts/r/android/restart_app.py
@@ -7,7 +7,7 @@ from _android import *
 
 pkg = r"{{PKG_NAME}}"
 
-call2("adb logcat -c")
+# call2("adb logcat -c")
 
 restart_app(pkg)
 
--- scripts/r/tools/sharex.py
+++ scripts/r/tools/sharex.py
@@ -1,43 +1,5 @@
 from _shutil import *
 
-if platform.system() != "Windows":
-    sys.exit(0)
-
-
-def install_snipaste():
-    url = "https://dl.snipaste.com/win-x64"
-    install_dir = "C:\\Snipaste"
-
-    if not exists(install_dir):
-        chdir(gettempdir())
-        download(url, "Snipaste.zip", redownload=True)
-        unzip("Snipaste.zip", install_dir)
-
-    with open(install_dir + "\\config.ini", "w") as f:
-        f.write(
-            """[General]
-language=en
-startup_fix=true
-first_run=false
-
-[Hotkey]
-delayed_snip=
-snip="16777264, 112"
-paste=
-hide=
-switch=
-snip_and_copy=
-
-[Snip]
-ask_for_confirm_on_esc=false
-
-[Update]
-check_on_start=false
-"""
-        )
-
-    call(f"start {install_dir}\\Snipaste.exe")
-
 
 def kill_proc():
     while subprocess.call('tasklist | find "ShareX.exe"', shell=True) == 0:
@@ -65,6 +27,7 @@ def install_sharex():
     ] = "%yy%mo%d%h%mi%s_%ms"
     # config["DefaultTaskSettings"]["CaptureSettings"]["FFmpegOptions"]
     config["DefaultTaskSettings"]["CaptureSettings"]["ShowCursor"] = False
+    config["AutoCheckUpdate"] = False
     json.dump(config, open(config_file, "w"))
 
     # config_file = os.path.join(setting_path, "HotkeysConfig.json")
@@ -76,5 +39,6 @@ def install_sharex():
     subprocess.Popen([exe_path, "-silent"], close_fds=True)
 
 
-# install_snipaste()
-install_sharex()
+if __name__ == "__main__":
+    if sys.platform() == "win32":
+        install_sharex()
