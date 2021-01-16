5b4cee968d96820982e184b184a426d1cdd22f40
rossning92 <rossning92@github.com>
2021-01-16 14:31:31 -0800

    commit with no message.

--- scripts/r/web/animation/_framework/src/yo.ts
+++ scripts/r/web/animation/_framework/src/yo.ts
@@ -53,23 +53,6 @@ let camera = null;
 let lightGroup = null;
 let cameraControls = null;
 let palette = [
-  // '#1abc9c',
-  // '#2ecc71',
-  // '#3498db',
-  // '#9b59b6',
-  // '#34495e',
-  // '#16a085',
-  // '#27ae60',
-  // '#2980b9',
-  // '#8e44ad',
-  // '#2c3e50',
-  // '#f1c40f',
-  // '#e67e22',
-  // '#e74c3c',
-  // '#f39c12',
-  // '#d35400',
-  // '#c0392b'
-
   "#1a535c",
   "#4ecdc4",
   "#ff6b6b",
