5244b4539da7e2aa8d815e94c39af52a8e4bedad
rossning92 <rossning92@github.com>
2021-02-04 19:08:54 -0800

    commit with no message.

--- scripts/r/video/to_gif.py
+++ scripts/r/video/to_gif.py
@@ -4,40 +4,43 @@ import subprocess
 from _appmanager import *
 from _shutil import *
 
-get_executable("magick")
-
 
 fps = "{{_FPS}}" if "{{_FPS}}" else 15
 
-f = os.environ["_FILE"]
-os.chdir(os.path.dirname(f))
-file_name = os.path.basename(f)
+cur_folder = get_current_folder()
+mkdir(os.path.join(cur_folder, "out"))
+cd(cur_folder)
 
-# Convert video to gif
-# fps=25,scale=w=-1:h=480
-out_gif = os.path.splitext(file_name)[0] + ".gif"
-args = [f"ffmpeg", "-i", file_name, "-filter_complex"]
+for f in get_files(cd=True):
+    file_name = os.path.basename(f)
 
-# Filter complex
-filter = f"[0:v] fps={fps}"
-if "{{_SCALE_H}}":
-    filter += ",scale=-1:{{_SCALE_H}}"
-filter += ",split [a][b];"
+    # Convert video to gif
+    # fps=25,scale=w=-1:h=480
+    out_gif = os.path.join("out", os.path.splitext(file_name)[0] + ".gif")
+    args = [f"ffmpeg", "-i", file_name, "-filter_complex"]
 
-if "{{_SINGLE_PALETTE}}":
-    filter += "[a] palettegen [p];[b][p] paletteuse"
-else:
-    filter += "[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1"
+    # Filter complex
+    filter = f"[0:v] fps={fps}"
+    if "{{_SCALE_H}}":
+        filter += ",scale=-1:{{_SCALE_H}}"
+    filter += ",split [a][b];"
 
-args.append(filter)
+    if "{{_SINGLE_PALETTE}}":
+        filter += "[a] palettegen [p];[b][p] paletteuse"
+    else:
+        filter += "[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1"
 
-args += [out_gif, "-y"]
+    args.append(filter)
 
-call_echo(args)
+    args += [out_gif, "-y"]
 
-# Optimize gif
-if "{{_OPTIMIZE_GIF}}":
-    print2("Optimize gif...")
-    out_gif_optimized = os.path.splitext(f)[0] + "_optimized.gif"
-    args = f'magick "{out_gif}" -coalesce -fuzz 4%% +dither -layers Optimize "{out_gif_optimized}"'
-    call2(args)
+    call_echo(args)
+
+    # Optimize gif
+    if "{{_OPTIMIZE_GIF}}":
+        print2("Optimize gif...")
+        out_gif_optimized = os.path.join(
+            "out", os.path.splitext(f)[0] + ".optimized.gif"
+        )
+        args = f'magick "{out_gif}" -coalesce -fuzz 4%% +dither -layers Optimize "{out_gif_optimized}"'
+        call2(args)
