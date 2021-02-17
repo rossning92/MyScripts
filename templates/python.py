4cd4aa2421a12744def3898e2cbe5114fb7226b2
Ross Ning <rossning92@gmail.com>
2021-02-16 16:56:46 -0800

    commit with no message.

--- templates/python.py
+++ templates/python.py
@@ -15,20 +15,16 @@ import os
 import sys
 
 
-def main(arguments):
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("infile", help="Input file", type=argparse.FileType("r"))
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        help="Output file",
+        default=sys.stdout,
+        type=argparse.FileType("w"),
+    )
+    parser.add_argument("--foobar", action="store_true")
 
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('infile', help="Input file",
-                        type=argparse.FileType('r'))
-    parser.add_argument('-o', '--outfile', help="Output file",
-                        default=sys.stdout, type=argparse.FileType('w'))
-
-    args = parser.parse_args(arguments)
-
-    print(args)
-
-
-if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    args = parser.parse_args()
