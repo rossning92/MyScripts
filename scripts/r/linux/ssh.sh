cf8a380e54c5ffad27c4685be5196a7ff89e3c82
Ross Ning <rossning92@gmail.com>
2021-03-15 01:19:46 -0700

    commit with no message.

--- scripts/r/linux/ssh.sh
+++ scripts/r/linux/ssh.sh
@@ -5,16 +5,18 @@ fi
 
 printf "Host *\nControlMaster auto\nControlPath ~/.ssh/master-%%r@%%h:%%p.socket\n" >~/.ssh/config
 
-sudo /etc/init.d/screen-cleanup start
-screen -r ssh_session -X quit
-screen -dmS ssh_session bash
+ssh -o "StrictHostKeyChecking no" {{_USER}}@{{_HOST}}
 
-if [[ -z "{{_PWD}}" ]]; then
-    screen -r ssh_session -X stuff "ssh {{_USER}}@{{_HOST}}
-"
-else
-    screen -r ssh_session -X stuff "expect -c 'set timeout -1; spawn ssh -o \"StrictHostKeyChecking no\" -R 5037:localhost:5037 {{_USER}}@{{_HOST}}; expect \"password:\"; send \"{{_PWD}}\r\"; interact;'
-"
-fi
+# sudo /etc/init.d/screen-cleanup start
+# screen -r ssh_session -X quit
+# screen -dmS ssh_session bash
 
-screen -r ssh_session
+# if [[ -z "{{_PWD}}" ]]; then
+#     screen -r ssh_session -X stuff "ssh {{_USER}}@{{_HOST}}
+# "
+# else
+#     screen -r ssh_session -X stuff "expect -c 'set timeout -1; spawn ssh -o \"StrictHostKeyChecking no\" -R 5037:localhost:5037 {{_USER}}@{{_HOST}}; expect \"password:\"; send \"{{_PWD}}\r\"; interact;'
+# "
+# fi
+
+# screen -r ssh_session
--- scripts/r/my_git_config.sh
+++ scripts/r/my_git_config.sh
@@ -1,3 +1,4 @@
 git config --global credential.helper cache
 git config --global credential.helper wincred
 git config --global core.autocrlf false
+git config --global core.longpaths true
