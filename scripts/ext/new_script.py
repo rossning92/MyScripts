b027bda33a644bf8b83e8d514c6ed4ab99e63827
Ross Ning <rossning92@gmail.com>
2021-03-29 11:33:09 -0700

    commit with no message.

--- scripts/ext/new_script.py
+++ scripts/ext/new_script.py
@@ -1,28 +1,26 @@
-from _gui import gui_input
 from _editor import *
 from _ext import *
 
 
-os.chdir('../')
+os.chdir("../")
 rel_path = get_selected_script_dir_rel()
 
-script_path = gui_input('Script name:', rel_path)
+script_path = input("script name: ")
 if script_path:
     dir_name = os.path.dirname(script_path)
-    if dir_name != '':
+    if dir_name != "":
         os.makedirs(dir_name, exist_ok=True)
 
     name, ext = os.path.splitext(script_path)
     if not ext:
-        print('Please specify script extension')
+        print("Please specify script extension")
         exit(0)
 
-    if ext == '.py':
-        shutil.copyfile(get_my_script_root() + '/templates/python.py',
-                        script_path)
+    if ext == ".py":
+        shutil.copyfile(get_my_script_root() + "/templates/python.py", script_path)
     else:
         # Create empty file
-        with open(script_path, 'w') as f:
+        with open(script_path, "w") as f:
             pass
 
     edit_myscript_script(os.path.realpath(script_path))
