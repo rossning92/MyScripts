59bd567c82b333eec2d5de8099e81a9ad291b307
Ross Ning <rossning92@gmail.com>
2021-03-30 15:56:25 -0700

    commit with no message.

--- scripts/r/android/restart_app.py
+++ scripts/r/android/restart_app.py
@@ -2,14 +2,18 @@ from _shutil import *
 from _script import *
 from _android import *
 
-# Run app using monkey:
-# adb shell monkey -p your.app.package.name -c android.intent.category.LAUNCHER 1
+if __name__ == "__main__":
+    # Run app using monkey:
+    # adb shell monkey -p your.app.package.name -c android.intent.category.LAUNCHER 1
 
-pkg = r"{{PKG_NAME}}"
+    if len(sys.argv) == 2:
+        pkg = sys.argv[1]
+    else:
+        pkg = r"{{PKG_NAME}}"
 
-# call2("adb logcat -c")
+    # call2("adb logcat -c")
 
-restart_app(pkg, use_monkey=False)
+    restart_app(pkg, use_monkey=False)
 
-if "{{_SHOW_LOGCAT}}":
-    logcat(proc_name=pkg)
+    if "{{_SHOW_LOGCAT}}":
+        logcat(proc_name=pkg)
