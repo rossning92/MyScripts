37e712bda2d82e7c77ba91c6cdd162edd6a76b62
Ross Ning <rossning92@gmail.com>
2021-03-31 15:25:36 -0700

    commit with no message.

--- scripts/r/android/install_apk.py
+++ scripts/r/android/install_apk.py
@@ -1,18 +1,21 @@
 from _shutil import *
 import re
-from _android import setup_android_env
 from _android import *
 
-file = os.environ['_FILE']
-assert os.path.splitext(file)[1].lower() == '.apk'
 
-print('APK file: %s' % file)
-adb_install2(file)
+files = get_files()
 
-# Run app
-pkg = get_pkg_name_apk(file)
-try:
-    start_app(pkg)
-except:
-    print2('ERROR: start app failed.', color='red')
-logcat(pkg)
+for file in files:
+    assert os.path.splitext(file)[1].lower() == ".apk"
+
+    print("APK file: %s" % file)
+    adb_install2(file)
+
+    if len(files) == 1:
+        # Run app
+        pkg = get_pkg_name_apk(file)
+        try:
+            start_app(pkg)
+        except:
+            print2("ERROR: start app failed.", color="red")
+        logcat(pkg)
