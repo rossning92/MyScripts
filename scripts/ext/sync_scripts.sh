e34f7ce6d7a85a28efd95d84015daf90923ccd10
rossning92 <rossning92@github.com>
2021-04-15 18:45:26 -0700

    commit with no message.

--- scripts/ext/sync_scripts.sh
+++ scripts/ext/sync_scripts.sh
@@ -6,26 +6,25 @@ git config --global user.email "rossning92@github.com"
 git config --global user.name "rossning92"
 git config credential.helper store
 
-
 if [ ! -d ".git" ]; then
-	git init
-	git remote add origin https://github.com/rossning92/MyScripts
-	git fetch
-	git reset --hard origin/master
-	git branch --set-upstream-to=origin/master master
+    git init
+    git remote add origin https://github.com/rossning92/MyScripts
+    git fetch
+    git reset --hard origin/master
+    git branch --set-upstream-to=origin/master master
 fi
 
+# Check if file is modified
 git status --short
 status=$(git status --short)
-if [[ ! -z "$status" ]]; then
-    echo "Press ENTER to commit those files..."
-    read -n 1 -s -r input
-    
-    if [[ -z $input ]]; then
-        git add -A
-        git commit -m 'commit with no message.'
-    fi
-fi
+[[ -z "$status" ]] && exit 0
+
+echo "Press y to commit..."
+read -n1 ans
+[[ "$ans" != "y" ]] && exit 0
+
+git add -A
+git commit -m 'commit with no message.'
 
 git pull --rebase
 # git submodule update --recursive --remote
