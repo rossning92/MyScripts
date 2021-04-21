9de30d2bfd4ac1a462cd71ac23d3fc15552bad1b
rossning92 <rossning92@github.com>
2021-04-21 00:18:57 -0700

    commit with no message.

--- scripts/r/web/animation/uiautomate/ipython.py
+++ scripts/r/web/animation/uiautomate/ipython.py
@@ -3,12 +3,15 @@ import os
 from _script import wt_wrap_args
 from _shutil import *
 from r.web.animation.record_screen import recorder
-from .uiautomate import *
+from uiautomate import *
 
 root = os.path.dirname(os.path.abspath(__file__))
 
 
 def open_wt_ipython(startup=None):
+    run_ahk(os.path.join(root, "show_overlay.ahk"))
+    time.sleep(2)
+
     startup_file = os.path.join(root, "_ipython_prompt.py")
     s = open(startup_file, "r").read()
     if startup is not None:
@@ -20,29 +23,11 @@ def open_wt_ipython(startup=None):
     args = wt_wrap_args(
         args,
         title="ross@ross-desktop2: IPython",
-        font_size=18,
+        font_size=14,
         icon=(root + "/python.ico").replace("\\", "/"),
         opacity=0.9,
     )
     call_echo(args)
-
-
-def record_ipython(file, func, startup=None):
-    if os.path.exists(file):
-        return file
-
-    else:
-        return record_ipython2(file, func, startup=startup)
-
-
-def record_ipython2(file, func, startup=None):
-    call_echo(["powershell", "-command", "Set-WinUserLanguageList -Force 'en-US'"])
-
-    run_ahk(os.path.join(root, "show_overlay.ahk"))
-    time.sleep(2)
-
-    open_wt_ipython(startup=startup)
-    time.sleep(2)
 
     exec_ahk(
         """
@@ -51,23 +36,8 @@ def record_ipython2(file, func, startup=None):
         SetWindowPos("A", 240, 125, 1440, 810)
         """
     )
-    sleep(0.5)
+    sleep(1)
 
-    recorder.set_region((0, 0, 1920, 1080))
 
-    recorder.start_record(file)
-
-    func()
-
-    sleep(2)
-    recorder.stop_record()
-
-    send_hotkey("alt", "f4")
-    exec_ahk("WinClose, show_overlay.ahk")
-
-    call_echo(
-        ["powershell", "-command", "Set-WinUserLanguageList -Force 'zh-CN', 'en-US'"]
-    )
-
-    return file
-
+if __name__ == "__main__":
+    open_wt_ipython()
