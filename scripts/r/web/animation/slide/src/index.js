05f79d68b7752845ff7437b193fcd1523ea226f7
rossning92 <rossning92@gmail.com>
2021-05-01 16:29:28 -0700

    commit with no message.

--- scripts/r/web/animation/slide/src/index.js
+++ scripts/r/web/animation/slide/src/index.js
@@ -5,4 +5,6 @@ require("./markdown.css");
 import example from "./example.md";
 import marked from "marked";
 
+console.log(MARKDOWN);
+
 document.body.innerHTML = marked(MARKDOWN ? MARKDOWN : example);
--- scripts/r/web/animation/slide/webpack.config.js
+++ scripts/r/web/animation/slide/webpack.config.js
@@ -3,38 +3,41 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "index_bundle.js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: "src/markdown.html" }),
-    new MiniCssExtractPlugin(),
-    new webpack.DefinePlugin({
-      MARKDOWN: JSON.stringify("# 你好"),
-    }),
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    open: true,
-    watchContentBase: true,
-    hot: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader, // instead of style-loader
-          "css-loader",
-        ],
-      },
-      {
-        test: /\.(html|md)$/,
-        loader: "raw-loader",
-      },
+module.exports = ({ markdown }) => {
+  return {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "index_bundle.js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "src/markdown.html" }),
+      new MiniCssExtractPlugin(),
+      new webpack.DefinePlugin({
+        MARKDOWN: JSON.stringify(markdown),
+      }),
     ],
-  },
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      open: true,
+      watchContentBase: true,
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader, // instead of style-loader
+            "css-loader",
+          ],
+        },
+        {
+          test: /\.(html|md)$/,
+          loader: "raw-loader",
+        },
+      ],
+    },
+  };
 };
