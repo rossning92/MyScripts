36c5e3566c8649ac5dabb5e5be3d34c92fd98e93
Ross Ning <rossning92@gmail.com>
2021-05-05 15:15:06 -0700

    commit with no message.

--- scripts/ext/copy_script_path.py
+++ scripts/ext/copy_script_path.py
@@ -1,12 +1,18 @@
 from _shutil import *
 
-script_root = os.path.realpath(os.path.realpath(__file__) + '/../../')
 
-script_path = os.environ['_SCRIPT']
+script_path = os.environ["_SCRIPT"]
 
-# Get relative path
-script_path = re.sub('^' + re.escape(script_root), '', script_path)
-script_path = script_path.replace('\\', '/')
+if script_path.endswith(".md"):
+    with open(script_path, "r", encoding="utf-8") as f:
+        set_clip(f.read())
 
-print('Script path copied: %s' % script_path)
-set_clip(script_path)
+    print("Content is copied to clipboard.")
+else:
+    # Copy relative path
+    script_root = os.path.realpath(os.path.realpath(__file__) + "/../../")
+    script_path = re.sub("^" + re.escape(script_root), "", script_path)
+    script_path = script_path.replace("\\", "/")
+
+    set_clip(script_path)
+    print("Script path copied: %s" % script_path)
