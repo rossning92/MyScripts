223e4d201a23585c8ce7977ab08e0222da9f8a3c
rossning92 <rossning92@gmail.com>
2021-06-04 15:10:40 -0700

    commit with no message.

--- libs/_shutil.py
+++ libs/_shutil.py
@@ -894,6 +894,12 @@ def setup_nodejs(install=True):
         print("setup_nodejs() not supported for current OS. Ignored.")
 
 
+def npm_install(path="."):
+    path = os.path.abspath(path)
+    if not os.path.exists(os.path.join(path, "node_modules")):
+        call_echo("yarn", cwd=path)
+
+
 def get_next_file_name(file):
     POSTFIX_START = "_001"
 
