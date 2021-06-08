c40cd2379c70ca8a1bcd76d2857decbfc4ad4a77
Ross Ning <rossning92@gmail.com>
2021-06-08 13:06:38 -0700

    commit with no message.

---
+++ scripts/r/android/install_adb_linux.sh
@@ -0,0 +1,3 @@
+cd ~
+wget https://dl.google.com/android/repository/platform-tools_r31.0.2-linux.zip
+unzip platform-tools_r31.0.2-linux.zip
--- scripts/r/android/install_android_sdk_linux.sh
+++ scripts/r/android/install_android_sdk_linux.sh
@@ -2,19 +2,18 @@ set -e
 
 cd ~
 
-
-# sudo apt update
 # sudo apt install android-sdk -y
-# sudo apt install unzip -y
 
+sudo apt update
+sudo apt install unzip -y
 
-# wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
-# unzip sdk-tools-linux-*.zip
-# rm sdk-tools-linux-*.zip
+wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+unzip sdk-tools-linux-*.zip
+rm sdk-tools-linux-*.zip
 
 cd ~/tools/bin
 
-# ./sdkmanager --update
+./sdkmanager --update
 ./sdkmanager "build-tools;27.0.3"
-./sdkmanager "lldb;3.1"
-./sdkmanager "ndk-bundle"
+# ./sdkmanager "lldb;3.1"
+# ./sdkmanager "ndk-bundle"
