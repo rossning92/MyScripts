ea2a5ee77cc2a1ef4b10cfa7c3649b7aba1a4479
Ross Ning <rossning92@gmail.com>
2021-06-24 19:46:31 -0700

    commit with no message.

--- scripts/r/video/add_title.sh
+++ scripts/r/video/add_title.sh
@@ -1,1 +1,11 @@
-ffmpeg -i "$1" -filter_complex "[0:v]pad=iw:ih+50:0:50:color=white, drawtext=text='{{_TITLE}}':fix_bounds=true:fontfile=/Windows/Fonts/arial.ttf:fontsize=18:fontcolor=black:x=(w-tw)/2:y=(50-th)/2" 123.mp4 -y
+set -e
+
+folder=$(dirname "$1")
+file=$(basename "$1")
+
+cd "$folder"
+mkdir -p out
+
+text=$(<label.txt)
+
+ffmpeg -i "$1" -filter_complex "[0:v]pad=iw:ih+50:0:50:color=white, drawtext=text='${text}':fix_bounds=true:fontfile=/Windows/Fonts/arial.ttf:fontsize=18:fontcolor=black:x=(w-tw)/2:y=(50-th)/2" "out/$file" -y
