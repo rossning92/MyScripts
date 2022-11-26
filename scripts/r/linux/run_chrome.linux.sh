81842408bfd078b8fe1edd85331e3a710c368698
Ross Ning <rossning92@gmail.com>
2022-11-26 14:47:47 -0500

    No commit message

--- scripts/r/linux/run_chrome.linux.sh
+++ scripts/r/linux/run_chrome.linux.sh
@@ -1,7 +1,7 @@
 if ! [ -x "$(command -v google-chrome)" ]; then
-    cd ~/tmp
+    cd /tmp/
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo apt install ./google-chrome-stable_current_amd64.deb
+    sudo apt install ./google-chrome-stable_current_amd64.deb -y
     rm google-chrome-stable_current_amd64.deb
 fi
 
