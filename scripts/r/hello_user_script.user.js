31ce1e97c7f5d524b8f85cefb7909159454ece39
Ross Ning <rossning92@gmail.com>
2023-01-23 00:07:08 -0800

    No commit message

--- scripts/r/hello_user_script.user.js
+++ scripts/r/hello_user_script.user.js
@@ -7,22 +7,25 @@
 // @author      -
 // @description 9/10/2022, 2:52:23 PM
 // @require https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@2,npm/@violentmonkey/ui@0.7
+// @require https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
 // @require ../../jslib/_userscript.js
 // ==/UserScript==
 
-let btn = document.createElement("button");
-btn.innerHTML = "Click Me";
-btn.onclick = function () {
-  findText("Terms");
-};
-
-const panel = VM.getPanel({
-  content: btn,
-  theme: "dark",
+VM.shortcut.register("c-i", () => {
+  alert("c-i pressed");
 });
-panel.wrapper.style.top = "0";
 
-panel.setMovable(true);
+(function createPanel() {
+  let btn = document.createElement("button");
+  btn.innerHTML = "Click Me";
+  btn.onclick = function () {
+    findText("Terms");
+  };
 
-// Show panel
-panel.show();
+  const panel = VM.getPanel({
+    content: btn,
+  });
+  panel.wrapper.style.top = "0";
+  panel.setMovable(true);
+  panel.show();
+})();
