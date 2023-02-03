fe771fd861671f03becfbb70ef761bd73600596e
Ross Ning <rossning92@gmail.com>
2023-02-03 09:53:24 -0800

    No commit message

--- scripts/r/android/disable_selinux.sh
+++ scripts/r/android/disable_selinux.sh
@@ -4,10 +4,11 @@ out="$(adb shell getenforce | tr -d '\r')"
 if [ "$out" != "Permissive" ]; then
     adb root
     adb shell "setenforce 0"
-    adb shell "stop;start"
 
-    run_script wait_hmd_to_boot
-    run_script disable_prox
+    # No need: the above takes effect immediately
+    # adb shell "stop;start"
+    # run_script wait_hmd_to_boot
+    # run_script disable_prox
 else
     echo 'SELinux is not enforced.'
 fi
