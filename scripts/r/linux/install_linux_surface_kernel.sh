9bab50dc67a46e6bd7db1b50d706cfe607d2fad2
Ross Ning <rossning92@gmail.com>
2023-07-02 16:58:59 -0700

    No commit message

--- scripts/r/linux/install_linux_surface_kernel.sh
+++ scripts/r/linux/install_linux_surface_kernel.sh
@@ -1,11 +1,20 @@
-# https://github.com/linux-surface/linux-surface/wiki/Installation-and-Setup#manually-installing-the-repository
-
 set -e
-wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc |
-    gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/linux-surface.gpg
-echo "deb [arch=amd64] https://pkg.surfacelinux.com/debian release main" |
-    sudo tee /etc/apt/sources.list.d/linux-surface.list
-sudo apt update
-sudo apt install linux-image-surface linux-headers-surface libwacom-surface iptsd
-# sudo apt install linux-surface-secureboot-mok -y
-sudo update-grub
+
+if [[ "$(uname -r)" != *"-surface"* ]]; then
+    # https://github.com/linux-surface/linux-surface/wiki/Installation-and-Setup#manually-installing-the-repository
+    wget -qO - https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc |
+        gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/linux-surface.gpg
+    echo "deb [arch=amd64] https://pkg.surfacelinux.com/debian release main" |
+        sudo tee /etc/apt/sources.list.d/linux-surface.list
+    sudo apt update
+    sudo apt install linux-image-surface linux-headers-surface libwacom-surface iptsd
+    # sudo apt install linux-surface-secureboot-mok -y
+    sudo update-grub
+fi
+
+# Config thermald
+# https://github.com/linux-surface/linux-surface/tree/master/contrib/thermald
+echo 'Configure thermald...'
+sudo cp "$(dirname "$0")/../../../settings/linux-surface/thermal-conf.xml" /etc/thermald/thermal-conf.xml
+sudo systemctl restart thermald.service
+sudo systemctl status thermald.service
