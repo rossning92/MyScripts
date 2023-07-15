b00e0c0b19f2145c3533b07bf81194033883b10a
Ross Ning <rossning92@gmail.com>
2023-07-15 11:40:22 -0700

    No commit message

--- scripts/r/linux/ssh_copy_id.sh
+++ scripts/r/linux/ssh_copy_id.sh
@@ -3,4 +3,6 @@ if [ ! -f ~/.ssh/id_rsa.pub ]; then
     ssh-keygen -t rsa
 fi
 set -x
-ssh-copy-id -p {{SSH_PORT}} {{SSH_USER}}@{{SSH_HOST}}
+if ! ssh -q -o "BatchMode=yes" {SSH_USER}}@{{SSH_HOST}} exit; then
+    ssh-copy-id -p {{SSH_PORT}} {{SSH_USER}}@{{SSH_HOST}}
+fi
