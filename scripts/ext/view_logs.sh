3a37aec93e66ab6eea9c9cb19b59d2e83d6faedb
Ross Ning <rossning92@gmail.com>
2023-08-28 19:53:50 -0700

    No commit message

--- scripts/ext/view_logs.sh
+++ scripts/ext/view_logs.sh
@@ -1,3 +1,3 @@
 set -e
 log_file="${MY_DATA_DIR}/MyScripts.log"
-tail +1f "$log_file" | run_script r/highlight.py -p "^.*? D .*=gray" "^.*? W .*=yellow" "^.*? E .*=red"
+tail -n 100 -f "$log_file" | run_script r/highlight.py -p "^.*? D .*=gray" "^.*? W .*=yellow" "^.*? E .*=red"
