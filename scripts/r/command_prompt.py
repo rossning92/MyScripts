3c857a1b966cf9d9dc4e9f26a2018a0b0866d208
Ross Ning <rossning92@gmail.com>
2023-09-20 20:16:31 -0700

    No commit message

--- scripts/r/command_prompt.py
+++ scripts/r/command_prompt.py
@@ -2,6 +2,7 @@ import logging
 import subprocess
 import sys
 
+from _android import setup_android_env
 from _cpp import setup_cmake
 from _shutil import setup_logger, setup_nodejs
 
@@ -11,6 +12,11 @@ if __name__ == "__main__":
     setup_cmake(install=False)
 
     setup_nodejs(install=False)
+
+    try:
+        setup_android_env()
+    except Exception as ex:
+        print(f"WARN: {ex}")
 
     if sys.platform == "win32":
         subprocess.call(["cmd", "/k", "echo."])
