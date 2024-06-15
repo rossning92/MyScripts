77c9e8db167fd38268484baba81f8adb9786f1a9
Ross Ning <rossning92@gmail.com>
2024-06-14 22:55:42 -0700

    No commit message

--- scripts/r/videoedit/link_vscode_extension.py
+++ scripts/r/videoedit/link_vscode_extension.py
@@ -2,13 +2,30 @@ import os
 import subprocess
 import sys
 
-if __name__ == "__main__":
+
+def link_vscode_extension():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    extension_path = os.path.join(script_dir, "_extension")
+
     if sys.platform == "win32":
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        extension_path = os.path.join(script_dir, "_extension")
         if not os.path.exists(extension_path):
-            raise Exception(f"extension path does not exists: {extension_path}")
+            raise Exception(f"VSCode extension path does not exist: {extension_path}")
         subprocess.call(
             f'MKLINK /J "%USERPROFILE%\\.vscode\\extensions\\videoedit" "{extension_path}"',
             shell=True,
         )
+    elif sys.platform == "linux":
+        subprocess.call(
+            [
+                "ln",
+                "-s",
+                extension_path,
+                os.path.expanduser("~/.vscode/extensions/videoedit"),
+            ],
+        )
+    else:
+        raise Exception("Unsupported platform: " + sys.platform)
+
+
+if __name__ == "__main__":
+    link_vscode_extension()
