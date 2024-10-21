9eeff30fad1e5c1ca8eb072971101fb50d1c23e1
Ross Ning <rossning92@gmail.com>
2024-10-21 09:37:20 -0700

    No commit message

--- scripts/r/android/toggle_sysprop.sh
+++ scripts/r/android/toggle_sysprop.sh
@@ -1,13 +1,15 @@
+{{val=SYSPROP_VAL if 'SYSPROP_VAL' in globals() else 1}}
 toggle_sysprop() {
     while true; do
         val=$(adb shell getprop $1 | tr -d '\r')
         read -n1 -p 'wait for key' ans
-        if [[ "$val" == '1' ]]; then
+        if [[ "$val" == '{{val}}' ]]; then
             echo "Set $1 = 0"
             adb shell setprop $1 0
         else
-            echo "Set $1 = 1"
-            adb shell setprop $1 1
+            echo "Set $1 = {{val}}"
+            adb shell setprop $1 {{val}}
         fi
     done
 }
+toggle_sysprop {{SYSPROP_NAME}}
