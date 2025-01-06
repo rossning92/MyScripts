2bd651e2dd06ef7bfa658c2f45f6ccd2e7198321
Ross Ning <rossning92@gmail.com>
2025-01-06 13:28:25 -0800

    No commit message

---
+++ scripts/r/pdf/extract_pages.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+import sys
+
+
+def split_pdf(file_path):
+    page_range = os.environ["_RANGE"]
+    base_name = os.path.splitext(file_path)[0]
+    out_file = f"{base_name}.p{page_range}.pdf"
+
+    subprocess.check_call(["pdftk", file_path, "cat", page_range, "output", out_file])
+
+
+if __name__ == "__main__":
+    split_pdf(file_path=sys.argv[1])
--- scripts/r/pdf/extract_pages.sh
+++
@@ -1,4 +0,0 @@
-set -e
-in_file="$1"
-out_file="${in_file%.*}.p{{_RANGE}}.pdf"
-pdftk "$1" cat {{_RANGE}} output "$out_file"
