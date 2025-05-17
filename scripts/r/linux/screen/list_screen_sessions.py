58f86e4e503f39fa7332058d7fa4ddfa7bbcb32c
Ross Ning <rossning92@gmail.com>
2025-05-17 11:28:08 -0700

    No commit message

---
+++ scripts/r/linux/screen/list_screen_sessions.py
@@ -0,0 +1,4 @@
+from _screenutils import select_screen
+
+if __name__ == "__main__":
+    id = select_screen()
--- scripts/r/linux/ssh_copy_id.sh
+++ scripts/r/linux/ssh_copy_id.sh
@@ -1,7 +1,7 @@
-# ssh-keygen
 if [ ! -f ~/.ssh/id_rsa.pub ]; then
+    echo 'No SSH key found. Generating new RSA key pair...'
     ssh-keygen -t rsa
 fi
 
-echo 'Copy SSH public key to remote server...'
+echo 'Copying SSH public key to remote server...'
 ssh-copy-id -o StrictHostKeyChecking=no -p {{SSH_PORT}} {{SSH_USER}}@{{SSH_HOST}}
