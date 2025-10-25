b6f9c03310f741b7d78c1e55ac426701cb368ef5
Ross Ning <rossning92@gmail.com>
2025-10-25 16:40:30 -0700

    No commit message

--- scripts/r/audio/concat.py
+++ scripts/r/audio/concat.py
@@ -1,9 +1,17 @@
-from utils.audio import *
+import argparse
+import os
+
+from utils.audio import concat_audio
 
 if __name__ == "__main__":
-    silence_secs = float("{{_SILENCE_SECS}}") if "{{_SILENCE_SECS}}" else 1.0
-    chdir(os.environ["CWD"])
-    audio_files = list(glob.glob("*.wav"))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files", nargs="+")
+    args = parser.parse_args()
 
-    mkdir("out")
-    concat_audio(audio_files, silence_secs, out_file="out/concat.wav")
+    silence_secs = float("{{_SILENCE_SECS}}") if "{{_SILENCE_SECS}}" else 1.0
+    audio_files = [os.path.abspath(path) for path in args.files]
+    base_dir = os.path.dirname(audio_files[0]) or "."
+    os.chdir(base_dir)
+
+    os.makedirs("tmp", exist_ok=True)
+    concat_audio(audio_files, silence_secs, out_file=os.path.join("tmp", "concat.wav"))
